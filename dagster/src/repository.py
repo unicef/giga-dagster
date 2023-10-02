822deb8cf61c3aca61bec9cbd1ce7c096d64c7cd
Kenneth V. Domingo <kenneth@thinkingmachin.es>
2023-10-02 17:56:17 +0800

    chore: remove delta poc from active repo list

--- dagster/src/repository.py
+++ dagster/src/repository.py
@@ -1,8 +1,9 @@
 from dagster import repository
-from src.jobs.delta_poc import delta_poc_job
+
+# from src.jobs.delta_poc import delta_poc_job
 
 
 @repository
 def giga_dataops_platform():
-    jobs = [delta_poc_job]
+    jobs = []
     return jobs
