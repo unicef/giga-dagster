c47f0a67871a87bbab1983f7619fcd0607b02944
tm-erin-cheng <erin@thinkingmachin.es>
2023-10-06 16:19:04 +0800

    add test assets

--- dagster/src/transforms/assets/great_expectations.py
+++ dagster/src/transforms/assets/great_expectations.py
@@ -3,21 +3,21 @@ from dagster_ge import ge_validation_op_factory
 from dagster import AssetKey, AssetsDefinition, asset
 
 expectation_suite_op = ge_validation_op_factory(
-    name="<your_asset_name>_expectations",
-    datasource_name="raw__bank_loan",
-    suite_name="<your_expectation_suite_name>",
+    name="bank_loan_expectations",
+    datasource_name="raw__bank_loans",
+    suite_name="bank_loan",
     validation_operator_name="action_list_operator",
 )
 
 expectation_suite_asset = AssetsDefinition.from_op(
     expectation_suite_op,
-    keys_by_input_name={"dataset": AssetKey("<your_asset_name>")},
+    keys_by_input_name={"dataset": AssetKey("raw__bank_loans")},
 )
 
 
 @asset(
     description="Do a full greatexpectations' data docs rebuild",
-    non_argument_deps={AssetKey("<your_asset_name>")},
+    non_argument_deps={AssetKey("raw__bank_loans")},
     required_resource_keys={"ge_data_context"},
     op_tags={"kind": "ge"},
 )
