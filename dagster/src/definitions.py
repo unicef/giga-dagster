f2f34e9a5e0abc4bbbdc5620e3ab36705bd46203
Kenneth V. Domingo <kenneth@thinkingmachin.es>
2023-10-09 22:23:29 +0800

    fix: io_manager definitions; address dependabot reports

--- dagster/src/definitions.py
+++ dagster/src/definitions.py
@@ -1,25 +1,28 @@
-import os
-
 from dagster_ge.factory import ge_data_context
 
 from dagster import Definitions, fs_io_manager
 from src.datasource1.assets.raw_bank_loans import raw__bank_loans
+from src.settings import ENVIRONMENT
 from src.transforms.assets.great_expectations import (
     expectation_suite_asset,
     ge_data_docs,
 )
 
-io_managers = {
-    "development": fs_io_manager.configured({"base_dir": "/tmp/io_manager_storage"}),
-}
+io_manager = (
+    {
+        "io_manager": fs_io_manager.configured({"base_dir": "/tmp/io_manager_storage"}),
+    }
+    if ENVIRONMENT != "production"
+    else {}
+)
 
 defs = Definitions(
     assets=[raw__bank_loans, expectation_suite_asset, ge_data_docs],
     resources={
-        "io_manager": io_managers.get(os.getenv("ENVIRONMENT")),
         "ge_data_context": ge_data_context.configured(
             {"ge_root_dir": "src/transforms/resources/great-expectations"}
         ),
+        **io_manager,
     },
     jobs=[],
     schedules=[],
