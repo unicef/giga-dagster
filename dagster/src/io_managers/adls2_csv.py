f480e22463786ccbc06e964aa633f50f171beed1
Kenneth V. Domingo <kenneth@thinkingmachin.es>
2023-10-18 11:07:21 +0800

    fix: use path prefix instead of filename

--- dagster/src/io_managers/adls2_csv.py
+++ dagster/src/io_managers/adls2_csv.py
@@ -9,7 +9,7 @@ from .common import get_dir_client, get_file_path_from_context
 
 
 class Adls2CsvIOManager(ConfigurableIOManager):
-    path_prefix = "raw"
+    path_prefix = "adls-test"
 
     @staticmethod
     def _type_handler(obj: pd.DataFrame) -> bytes:
@@ -18,14 +18,14 @@ class Adls2CsvIOManager(ConfigurableIOManager):
     def handle_output(self, context: OutputContext, obj: pd.DataFrame) -> None:
         text_obj = self._type_handler(obj)
         filename = f"{get_file_path_from_context(context)}.csv"
-        dir_client = get_dir_client(filename)
+        dir_client = get_dir_client(self.path_prefix)
         file_client: DataLakeFileClient = dir_client.create_file(filename)
         with io.BytesIO(text_obj) as buffer:
             file_client.upload_data(buffer, overwrite=True)
 
     def load_input(self, context: InputContext) -> pd.DataFrame:
         filename = get_file_path_from_context(context)
-        dir_client = get_dir_client(filename)
+        dir_client = get_dir_client(self.path_prefix)
         file_client = dir_client.get_file_client(f"{filename}.csv")
         file_stream: StorageStreamDownloader = file_client.download_file()
         with io.BytesIO() as buffer:
