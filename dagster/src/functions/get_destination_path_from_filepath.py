b7525a1cf8a23a24b7554a7d1d84c41d06e29d06
tm-sofia-pineda <sofia.pineda@thinkingmachin.es>
2023-10-24 17:09:41 +0800

    create function get_destination_path_from_filepath

---
+++ dagster/src/functions/get_destination_path_from_filepath.py
@@ -0,0 +1,31 @@
+filepath = "raw/school_geolocation/BEN-school_geolocation-20210923.csv"
+
+
+def get_destination_path_from_filepath(filepath):
+    if filepath.split("/", 1)[0] == "raw":
+        if filepath.split("/")[1] == "qos":
+            destination_prefix = "gold/qos"
+        else:
+            destination_prefix = "bronze"
+            print(destination_prefix)
+
+    elif filepath.split("/", 1)[0] == "bronze":
+        destination_prefix = "staging/pending_review"
+
+    elif filepath.split("/", 1)[0] == "staging/pending_review":
+        destination_prefix = "staging/approved"
+
+    elif filepath.split("/", 1)[0] == "staging/approved":
+        destination_prefix = "silver"
+
+    elif filepath.split("/", 1)[0] == "silver":
+        destination_prefix = "gold/master_data"
+
+    else:
+        print("unknown source prefix")
+
+    destination_filepath = destination_prefix + "/" + filepath.split("/", 1)[1]
+    return destination_filepath
+
+
+get_destination_path_from_filepath(filepath)
