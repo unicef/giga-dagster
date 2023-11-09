89b416f6b759ea0d28306d8ba9b124c7bf6c5801
tm-erin-cheng <erin@thinkingmachin.es>
2023-11-09 14:42:18 +0800

    add new assets with io manager

--- dagster/src/assets/transforms/assets.py
+++ dagster/src/assets/transforms/assets.py
@@ -1,64 +1,79 @@
 import pandas as pd
-from dagster_ge import ge_validation_op_factory
 
-from dagster import AssetKey, AssetsDefinition, OpExecutionContext, asset
+from dagster import OpExecutionContext, Output, asset
 
-
-@asset(required_resource_keys={"asset_loader"}, io_manager_key="adls_io_manager")
-def raw_file(context: OpExecutionContext) -> pd.DataFrame:
-    df = context.resources.asset_loader.load_file()
-    return df  # io manager should upload this to raw bucket as csv
+# from dagster_ge import ge_validation_op_factory
 
 
-@asset(
-    io_manager_key="adls_io_manager",
-)
-def bronze(context: OpExecutionContext, raw_file) -> pd.DataFrame:
-    return (
-        raw_file.transform()
-    )  # io manager should upload this to bronze/<dataset_name> as deltatable
+@asset(io_manager_key="adls_io_manager", required_resource_keys={"adls_loader"})
+def raw(context: OpExecutionContext) -> pd.DataFrame:
+    context.log.info(f'context run tags: {context.run_tags["dagster/run_key"]}')
 
+    df = context.resources.adls_loader.load_from_adls(
+        context.run_tags["dagster/run_key"]
+    )
+    context.log.info(f"data={df}")
 
-expectation_suite_op = ge_validation_op_factory(
-    name="raw_file_expectations",
-    datasource_name="raw_file",
-    suite_name="data-quality-checks",
-    validation_operator_name="action_list_operator",
-)
-
-expectation_suite_asset = AssetsDefinition.from_op(
-    expectation_suite_op,
-    keys_by_input_name={"dataset": AssetKey("bronze")},
-)  # not sure yet if we can return dfs from this. runs on dfs but uploads as deltatable
+    yield Output(df, metadata={"filepath": context.run_tags["dagster/run_key"]})
+    # return df  # io manager should upload this to raw bucket as csv
 
 
 @asset(
     io_manager_key="adls_io_manager",
 )
-def dq_passed_rows(
-    context: OpExecutionContext, expectations_suite_asset
-) -> pd.DataFrame:
-    return (
-        expectations_suite_asset.passed_rows()
-    )  # io manager should upload this to staging/pending-review/<dataset_name> as deltatable
+def bronze(context: OpExecutionContext, raw: pd.DataFrame) -> pd.DataFrame:
+    df = raw
+    df["d"] = 12345
+
+    yield Output(df, metadata={"filepath": context.run_tags["dagster/run_key"]})
+    # return Output(df, metadata={"filename": df.shape[0]})
+    # return (
+    #     raw_file.transform()
+    # )  # io manager should upload this to bronze/<dataset_name> as deltatable
+
+
+# expectation_suite_op = ge_validation_op_factory(
+#     name="raw_file_expectations",
+#     datasource_name="raw_file",
+#     suite_name="data-quality-checks",
+#     validation_operator_name="action_list_operator",
+# )
+
+# expectation_suite_asset = AssetsDefinition.from_op(
+#     expectation_suite_op,
+#     keys_by_input_name={"dataset": AssetKey("bronze")},
+# )  # not sure yet if we can return dfs from this. runs on dfs but uploads as deltatable
 
 
 @asset(
     io_manager_key="adls_io_manager",
 )
-def dq_failed_rows(
-    context: OpExecutionContext, expectations_suite_asset
-) -> pd.DataFrame:
-    return (
-        expectations_suite_asset.failed_rows()
-    )  # io manager should upload this to archive/gx_tests_failed as deltatable
+def dq_passed_rows(context: OpExecutionContext, bronze: pd.DataFrame) -> pd.DataFrame:
+    df = bronze
+    df["baby"] = "shark"
+    return df
+    # return (
+    #     expectations_suite_asset.passed_rows()
+    # )  # io manager should upload this to staging/pending-review/<dataset_name> as deltatable
+
+
+@asset(
+    io_manager_key="adls_io_manager",
+)
+def dq_failed_rows(context: OpExecutionContext, bronze: pd.DataFrame) -> pd.DataFrame:
+    df = bronze
+    df["doodoo"] = "doodoo"
+    return df
+    # return (
+    #     expectations_suite_asset.failed_rows()
+    # )  # io manager should upload this to archive/gx_tests_failed as deltatable
 
 
 @asset(
     io_manager_key="adls_io_manager",
 )
 def manual_review_passed_rows(
-    context: OpExecutionContext, dq_passed_rows
+    context: OpExecutionContext, dq_passed_rows: pd.DataFrame
 ) -> pd.DataFrame:
     return dq_passed_rows  # io manager should upload this to staging/approved/<dataset_name> as deltatable
 
@@ -67,7 +82,7 @@ def manual_review_passed_rows(
     io_manager_key="adls_io_manager",
 )
 def manual_review_failed_rows(
-    context: OpExecutionContext, dq_passed_rows
+    context: OpExecutionContext, dq_passed_rows: pd.DataFrame
 ) -> pd.DataFrame:
     return dq_passed_rows  # io manager should upload this to archive/manual_review_rejected as deltatable
 
@@ -79,15 +94,21 @@ def manual_review_failed_rows(
     io_manager_key="adls_io_manager",
 )
 def silver(context: OpExecutionContext, manual_review_passed_rows) -> pd.DataFrame:
-    return (
-        manual_review_passed_rows.transform()
-    )  # io manager should upload this to silver/<dataset_name> as deltatable
+    df = manual_review_passed_rows
+    df["e"] = False
+    return df
+    # return (
+    #     manual_review_passed_rows.transform()
+    # )  # io manager should upload this to silver/<dataset_name> as deltatable
 
 
 @asset(
     io_manager_key="adls_io_manager",
 )
-def gold(context: OpExecutionContext, silver) -> pd.DataFrame:
-    return (
-        silver.merge()
-    )  # io manager should upload this to gold/master_data as deltatable
+def gold(context: OpExecutionContext, silver: pd.DataFrame) -> pd.DataFrame:
+    df = silver
+    df["f"] = True
+    return df
+    # return (
+    #     silver.merge()
+    # )  # io manager should upload this to gold/master_data as deltatable
