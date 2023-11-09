77ed0367acb5c91bd75d55aee35587e266da017e
tm-erin-cheng <erin@thinkingmachin.es>
2023-11-09 14:39:38 +0800

    add jobs and sensors for adls

--- dagster/src/jobs.py
+++ dagster/src/jobs.py
@@ -1,18 +1,19 @@
-from dagster import define_asset_job, job
-from dagster.src.assets.transforms.delta_poc_op import write_delta_lake_poc
+from dagster import define_asset_job
+
+# from dagster.src.assets.transforms.delta_poc_op import write_delta_lake_poc
 
 
-@job
-def delta_poc_job():
-    write_delta_lake_poc()
+# @job
+# def delta_poc_job():
+#     write_delta_lake_poc()
 
 
 school_master__run_automated_data_checks_job = define_asset_job(
     name="school_master__run_automated_data_checks",
     selection=[
-        "raw_file",
+        "raw",
         "bronze",
-        "expectation_suite_asset",
+        # "expectation_suite_asset",
         "dq_failed_rows",
         "dq_passed_rows",
     ],
--- dagster/src/sensors.py
+++ dagster/src/sensors.py
@@ -1,28 +1,40 @@
 import os
 
-from dagster import RunRequest, sensor
-from dagster.src.jobs import (
+from dagster import Config, RunConfig, RunRequest, sensor
+from src._utils.io_managers import ADLSFileClient
+from src.jobs import (
     school_master__run_automated_data_checks_job,
     school_master__run_manual_checks_and_transforms_job,
 )
 
 
-@sensor(job=school_master__run_automated_data_checks_job)
-def school_master__run_automated_data_checks_sensor():
-    folders = [
-        "school-geolocation-data",
-        "school-coverage-data",
-        "infrastructure-data",
-        "geospatial-data",
-    ]
+class FileConfig(Config):
+    filepath: str
 
-    for folder in folders:
-        for filename in os.listdir(f"/raw/{folder}"):
-            filepath = os.path.join("/raw/", filename)
-            if os.path.isfile(filepath):
-                yield RunRequest(
-                    run_key=filename,
-                )
+
+@sensor(job=school_master__run_automated_data_checks_job, minimum_interval_seconds=60)
+def school_master__run_automated_data_checks_sensor():
+    adls = ADLSFileClient()
+
+    file_list = adls.list_paths("adls-testing-raw")
+
+    for file_data in file_list:
+        if file_data["is_directory"]:
+            continue
+        else:
+            filepath = file_data["name"]
+            print(f"FILE: {filepath}")
+            yield RunRequest(
+                run_key=f"{filepath}",
+                run_config=RunConfig(
+                    ops={
+                        "raw": FileConfig(filepath=filepath),
+                        "bronze": FileConfig(filepath=filepath),
+                        "dq_passed_rows": FileConfig(filepath=filepath),
+                        "dq_failed_rows": FileConfig(filepath=filepath),
+                    }
+                ),
+            )
 
 
 @sensor(job=school_master__run_manual_checks_and_transforms_job)
@@ -35,9 +47,9 @@ def school_master__run_manual_checks_and_transforms_sensor():
     ]
 
     for folder in folders:
-        for filename in os.listdir(f"/staging/pending-review/{folder}"):
-            filepath = os.path.join("/staging/pending-review", filename)
+        for filepath in os.listdir(f"/staging/pending-review/{folder}"):
+            filepath = os.path.join("/staging/pending-review", filepath)
             if os.path.isfile(filepath):
                 yield RunRequest(
-                    run_key=filename,
+                    run_key=filepath,
                 )
