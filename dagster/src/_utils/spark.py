bf837a58ac94981dfc9aa8ca51fef72a9bad05c8
tm-erin-cheng <erin@thinkingmachin.es>
2023-11-29 17:12:05 +0800

    fix spark

--- dagster/src/_utils/spark.py
+++ dagster/src/_utils/spark.py
@@ -1,53 +1,39 @@
 import pyarrow_hotfix  # noqa: F401
-from delta import configure_spark_with_delta_pip
-from pyspark import SparkConf
-from pyspark.sql import SparkSession
+from dagster_pyspark import PySparkResource
 
 from src.settings import settings
 
-
-def get_spark_session() -> SparkSession:
-    conf = SparkConf()
-    conf.set(
-        "spark.driver.extraJavaOptions",
-        " ".join(
+pyspark = PySparkResource(
+    spark_config={
+        "spark.app.name": f"giga-dagster@{settings.SHORT_SHA}",
+        "spark.master": f"spark://{settings.SPARK_MASTER_HOST}",
+        "spark.driver.extraJavaOptions": str.join(
+            " ",
             [
                 "-Divy.cache.dir=/tmp",
                 "-Divy.home=/tmp",
                 "-Dio.netty.tryReflectionSetAccessible=true",
-            ]
+            ],
         ),
-    )
-    conf.set("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension")
-    conf.set(
-        "spark.sql.catalog.spark_catalog",
-        "org.apache.spark.sql.delta.catalog.DeltaCatalog",
-    )
-    conf.set("spark.sql.execution.arrow.pyspark.enabled", "true")
-    conf.set("spark.sql.warehouse.dir", "/opt/spark/warehouse")
-    conf.set("spark.sql.catalogImplementation", "hive")
-    conf.set("spark.driver.cores", "1")
-    conf.set("spark.driver.memory", "512m")
-    conf.set("spark.executor.cores", "1")
-    conf.set("spark.executor.memory", "512m")
-    conf.set("spark.executor.instances", "1")
-    conf.set("spark.authenticate", "true")
-    conf.set("spark.authenticate.secret", settings.SPARK_RPC_AUTHENTICATION_SECRET)
-    conf.set("spark.authenticate.enableSaslEncryption", "true")
-    conf.set("spark.databricks.delta.properties.defaults.enableChangeDataFeed", "true")
-    conf.set("spark.databricks.delta.properties.defaults.appendOnly", "false")
-    # conf.set("spark.python.use.daemon", "true")
-    # conf.set("spark.python.daemon.module", "src.utils.sentry")
-    conf.set(
-        f"fs.azure.sas.{settings.AZURE_BLOB_CONTAINER_NAME}.{settings.AZURE_STORAGE_ACCOUNT_NAME}.blob.core.windows.net",
-        settings.AZURE_SAS_TOKEN,
-    )
-
-    builder = (
-        SparkSession.builder.master("spark://spark-master:7077")
-        .appName("GigaDagsterSpark")
-        .config(conf=conf)
-        .enableHiveSupport()
-    )
-    spark = configure_spark_with_delta_pip(builder)
-    return spark.getOrCreate()
+        "spark.sql.extensions": "io.delta.sql.DeltaSparkSessionExtension",
+        "spark.sql.catalog.spark_catalog": (
+            "org.apache.spark.sql.delta.catalog.DeltaCatalog"
+        ),
+        "spark.sql.execution.arrow.pyspark.enabled": "true",
+        "spark.sql.warehouse.dir": "/opt/spark/warehouse",
+        "spark.sql.catalogImplementation": "hive",
+        "spark.driver.cores": "1",
+        "spark.driver.memory": "512m",
+        "spark.executor.cores": "1",
+        "spark.executor.memory": "512m",
+        "spark.executor.instances": "1",
+        "spark.authenticate": "true",
+        "spark.authenticate.secret": settings.SPARK_RPC_AUTHENTICATION_SECRET,
+        "spark.authenticate.enableSaslEncryption": "true",
+        "spark.databricks.delta.properties.defaults.enableChangeDataFeed": "true",
+        "spark.databricks.delta.properties.defaults.appendOnly": "false",
+        f"fs.azure.sas.{settings.AZURE_BLOB_CONTAINER_NAME}.{settings.AZURE_STORAGE_ACCOUNT_NAME}.blob.core.windows.net": (
+            settings.AZURE_SAS_TOKEN
+        ),
+    }
+)
--- dagster/src/definitions.py
+++ dagster/src/definitions.py
@@ -1,10 +1,10 @@
 from dagster_ge.factory import ge_data_context
-from dagster_pyspark import PySparkResource
 
 from dagster import Definitions, load_assets_from_package_module
 from src import assets
 from src._utils.adls import ADLSFileClient
 from src._utils.sentry import setup_sentry
+from src._utils.spark import pyspark
 from src.jobs import (
     school_master__get_gold_delta_tables_job,
     school_master__run_automated_data_checks_job,
@@ -18,44 +18,9 @@ from src.sensors import (
     school_master__raw_file_uploads_sensor,
     school_master__successful_manual_checks_sensor,
 )
-from src.settings import settings
 
 setup_sentry()
 
-pyspark = PySparkResource(
-    spark_config={
-        "spark.app.name": f"giga-dagster@{settings.SHORT_SHA}",
-        "spark.master": f"spark://{settings.SPARK_MASTER_HOST}",
-        "spark.driver.extraJavaOptions": str.join(
-            " ",
-            [
-                "-Divy.cache.dir=/tmp",
-                "-Divy.home=/tmp",
-                "-Dio.netty.tryReflectionSetAccessible=true",
-            ],
-        ),
-        "spark.sql.extensions": "io.delta.sql.DeltaSparkSessionExtension",
-        "spark.sql.catalog.spark_catalog": (
-            "org.apache.spark.sql.delta.catalog.DeltaCatalog"
-        ),
-        "spark.sql.execution.arrow.pyspark.enabled": "true",
-        "spark.sql.warehouse.dir": "/opt/spark/warehouse",
-        "spark.sql.catalogImplementation": "hive",
-        "spark.driver.cores": "1",
-        "spark.driver.memory": "512m",
-        "spark.executor.cores": "1",
-        "spark.executor.memory": "512m",
-        "spark.executor.instances": "1",
-        "spark.authenticate": "true",
-        "spark.authenticate.secret": settings.SPARK_RPC_AUTHENTICATION_SECRET,
-        "spark.authenticate.enableSaslEncryption": "true",
-        "spark.databricks.delta.properties.defaults.enableChangeDataFeed": "true",
-        "spark.databricks.delta.properties.defaults.appendOnly": "false",
-        f"fs.azure.sas.{settings.AZURE_BLOB_CONTAINER_NAME}.{settings.AZURE_STORAGE_ACCOUNT_NAME}.blob.core.windows.net": (
-            settings.AZURE_SAS_TOKEN
-        ),
-    }
-)
 
 defs = Definitions(
     assets=[
