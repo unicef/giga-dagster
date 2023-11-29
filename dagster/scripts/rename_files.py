290503aef09836b4333b69cea6a55ff031bb47bf
tm-sofia-pineda <sofia.pineda@thinkingmachin.es>
2023-11-29 20:12:01 +0800

    feat: create script that rename files in silver

--- dagster/scripts/rename_files.py
+++ dagster/scripts/rename_files.py
@@ -1,7 +1,10 @@
 from src._utils.adls import ADLSFileClient
 
 gold_folder = "raw/school_geolocation_coverage_data/gold/school_data"
-silver_folder = "raw/school_geolocation_coverage_data/silver"
+silver_subfolders = [
+    "raw/school_geolocation_coverage_data/silver/school_data",
+    "raw/school_geolocation_coverage_data/silver/coverage_data",
+]
 bronze_folder = "raw/school_geolocation_coverage_data/bronze"
 
 adls = ADLSFileClient()
@@ -19,3 +22,28 @@ for file_data in gold_file_list:
     else:
         new_filepath = gold_folder + "/" + new_filename
         adls.rename_file(old_filepath=filepath, new_filepath=new_filepath)
+
+# Rename files in silver
+for folder in silver_subfolders:
+    silver_file_list = adls.list_paths(folder)
+    for file_data in silver_file_list:
+        if file_data["is_directory"]:
+            continue
+        else:
+            filepath = file_data["name"]
+            if (
+                "_school_geolocation_" not in filepath
+                and "_school-geolocation_coverage_" not in filepath
+            ):
+                continue
+            else:
+                if "_school_geolocation_" in filepath:
+                    new_filepath = filepath.replace(
+                        "_school_geolocation_", "_school-geolocation_"
+                    )
+                    print("***")
+                elif "_school-geolocation_coverage_" in filepath:
+                    new_filepath = filepath.replace(
+                        "_school-geolocation_coverage_", "_school-coverage_"
+                    )
+                adls.rename_file(old_filepath=filepath, new_filepath=new_filepath)
