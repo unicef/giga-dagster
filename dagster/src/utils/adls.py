a917f425f031836112b6f17b2f19c19372b46652
Kenneth V. Domingo <kenneth@thinkingmachin.es>
2023-12-06 18:28:46 +0800

    chore: remove old _utils folder

--- dagster/src/utils/adls.py
+++ dagster/src/utils/adls.py
@@ -110,6 +110,13 @@ class ADLSFileClient(ConfigurableResource):
         properties = file_client.get_file_properties()
         return properties
 
+    def rename_file(self, old_filepath: str, new_filepath: str):
+        file_client = _adls.get_file_client(file_path=old_filepath)
+        new_path = file_client.file_system_name + "/" + new_filepath
+        renamed_file_client = file_client.rename_file(new_name=new_path)
+        print(f"File {old_filepath} renamed to {new_path}")
+        return renamed_file_client
+
 
 def get_filepath(source_path: str, dataset_type: str, step: str):
     filename = source_path.split("/")[-1]
