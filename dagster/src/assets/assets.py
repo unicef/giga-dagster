b3d3150d4959b1683c6936515f3a5831eb602de7
tm-erin-cheng <erin@thinkingmachin.es>
2023-12-12 15:55:11 +0800

    feat: add temporary staging table - merge of silver and dq_passed

--- dagster/src/assets/assets.py
+++ dagster/src/assets/assets.py
@@ -1,12 +1,13 @@
 import pandas as pd
 from dagster_ge.factory import GEContextResource
 from dagster_pyspark import PySparkResource
+from delta.tables import DeltaTable
 from pyspark import sql
 
 import src.spark.transform_functions as tf
 from dagster import OpExecutionContext, Output, asset
 from src.resources.datahub_emitter import create_domains, emit_metadata_to_datahub
-from src.utils.adls import ADLSFileClient, get_output_filepath
+from src.utils.adls import ADLSFileClient, get_filepath, get_output_filepath
 from src.utils.ingest_azure_ad import run_azure_ad_to_datahub_pipeline
 
 
@@ -31,7 +32,7 @@ def raw(
     yield Output(df, metadata={"filepath": context.run_tags["dagster/run_key"]})
 
 
-@asset(io_manager_key="adls_bronze_io_manager")
+@asset(io_manager_key="adls_delta_io_manager")
 def bronze(context: OpExecutionContext, raw: sql.DataFrame) -> sql.DataFrame:
     emit_metadata_to_datahub(context, df=raw)
     df = tf.create_bronze_layer_columns(raw)
@@ -77,6 +78,8 @@ def dq_passed_rows(
     data_quality_results,
 ) -> sql.DataFrame:
     df_passed = tf.dq_passed_rows(bronze, data_quality_results)
+    df_passed = df_passed.drop("gx_index")
+    emit_metadata_to_datahub(context, df_passed)
     yield Output(df_passed, metadata={"filepath": get_output_filepath(context)})
 
 
@@ -87,8 +90,51 @@ def dq_failed_rows(
     data_quality_results,
 ) -> sql.DataFrame:
     df_failed = tf.dq_failed_rows(bronze, data_quality_results)
+    df_failed = df_failed.drop("gx_index")
     emit_metadata_to_datahub(context, df_failed)
     yield Output(df_failed, metadata={"filepath": get_output_filepath(context)})
+
+
+@asset(io_manager_key="adls_delta_io_manager")
+def staging(
+    context: OpExecutionContext,
+    df_passed: sql.DataFrame,
+    adls_file_client: ADLSFileClient,
+    spark: PySparkResource,
+) -> sql.DataFrame:
+    dataset_type = context.get_step_execution_context().op_config["dataset_type"]
+    silver_table_path = get_filepath(
+        context.run_tags["dagster/run_key"], dataset_type, "silver"
+    )
+
+    if DeltaTable.isDeltaTable(spark, silver_table_path):
+        # Clone silver table to staging folder
+        silver = adls_file_client.download_delta_table_as_spark_dataframe(
+            silver_table_path, spark.spark_session
+        )
+
+        staging_table_path = get_filepath(
+            context.run_tags["dagster/run_key"], dataset_type, "staging"
+        )
+        adls_file_client.upload_spark_dataframe_as_delta_table_within_dagster(
+            context, silver, staging_table_path, spark.spark_session
+        )
+
+        # Load silver table in staging folder as a deltatable
+        staging = adls_file_client.download_delta_table_as_delta_table(
+            staging_table_path, spark.spark_session
+        )
+
+        # Join silver table with df_passed (sql dataframe)
+        staging.alias("source").merge(
+            df_passed.alias("target"), "source.giga_id_school = target.giga_id_school"
+        ).whenMatchedUpdateAll().whenNotMatchedInsertAll().execute()
+        staging = staging.toDF()
+    else:
+        staging = df_passed
+
+    emit_metadata_to_datahub(context, staging)
+    yield Output(staging, metadata={"filepath": get_output_filepath(context)})
 
 
 @asset(io_manager_key="adls_delta_io_manager")
