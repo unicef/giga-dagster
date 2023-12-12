9d99afc6a7e019fdb2bd6f147431f132236bf36b
tm-erin-cheng <erin@thinkingmachin.es>
2023-12-12 15:52:14 +0800

    chore: remove adls_bronze

--- dagster/src/definitions.py
+++ dagster/src/definitions.py
@@ -2,18 +2,14 @@ from dagster_ge.factory import GEContextResource
 
 from dagster import Definitions, load_assets_from_package_module
 from src import assets
-from src.jobs import (  
+from src.jobs import (
     datahub__ingest_azure_ad_users_groups_job,
     school_master__automated_data_checks_job,
     school_master__convert_gold_csv_to_deltatable_job,
     school_master__failed_manual_checks_job,
     school_master__successful_manual_checks_job,
 )
-from src.resources.io_managers import (
-    ADLSBronzeIOManager,
-    ADLSDeltaIOManager,
-    ADLSRawIOManager,
-)
+from src.resources.io_managers import ADLSDeltaIOManager, ADLSRawIOManager
 from src.sensors import (
     school_master__failed_manual_checks_sensor,
     school_master__gold_csv_to_deltatable_sensor,
@@ -35,7 +31,6 @@ defs = Definitions(
     ],
     resources={
         "adls_raw_io_manager": ADLSRawIOManager(pyspark=pyspark),
-        "adls_bronze_io_manager": ADLSBronzeIOManager(pyspark=pyspark),
         "adls_delta_io_manager": ADLSDeltaIOManager(pyspark=pyspark),
         "adls_file_client": ADLSFileClient(),
         "gx": GEContextResource(ge_root_dir="src/resources/great_expectations"),
