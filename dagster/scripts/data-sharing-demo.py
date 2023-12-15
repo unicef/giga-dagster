6f16468a59e17138834583410ef4d53a53f8fcb3
tm-sofia-pineda <sofia.pineda@thinkingmachin.es>
2023-12-15 17:27:50 +0800

    feat: clone and update delta table

--- dagster/scripts/data-sharing-demo.py
+++ dagster/scripts/data-sharing-demo.py
@@ -1,25 +1,86 @@
-from pyspark.sql import functions as f
+from delta.tables import DeltaTable, SparkSession
+from pyspark import sql
+from pyspark.sql.functions import expr
 
 from src.settings import settings
+from src.utils.adls import ADLSFileClient
+from src.utils.spark import get_spark_session
+from src.utils.sql import load_sql_template
+
+adls_client = ADLSFileClient()
+
+
+def upload_spark_dataframe_as_delta_table(
+    data: sql.DataFrame, filepath: str, spark: SparkSession
+):
+    filename = filepath.split("/")[-1]
+    country_code = filename.split("_")[0]
+
+    # TODO: Get from context
+    schema_name = "school_data_v2"
+    create_schema_sql = load_sql_template(
+        "create_gold_schema",
+        schema_name=schema_name,
+    )
+    create_table_sql = load_sql_template(
+        "create_gold_table",
+        schema_name=schema_name,
+        table_name=country_code,
+        location=f"{settings.AZURE_BLOB_CONNECTION_URI}/{filepath}",
+    )
+    spark.sql(create_schema_sql)
+    spark.sql(create_table_sql)
+    data.write.format("delta").mode("overwrite").saveAsTable(
+        f"{schema_name}.{country_code}"
+    )
+
+
+def clone_table(spark: SparkSession, filepath, cloned_filepath):
+    df = adls_client.download_delta_table_as_spark_dataframe(
+        filepath=filepath, spark=spark
+    )
+    upload_spark_dataframe_as_delta_table(
+        data=df, filepath=cloned_filepath, spark=spark
+    )
+
+
+def download_delta_table_as_delta_table(filepath: str, spark: SparkSession):
+    adls_path = f"{settings.AZURE_BLOB_CONNECTION_URI}/{filepath}"
+
+    return DeltaTable.forPath(spark, f"{adls_path}")
+
+
+def update_values(adls_path, spark: SparkSession):
+    delta_table = download_delta_table_as_delta_table(
+        filepath=f"{adls_path}", spark=spark
+    )
+
+    delta_table.update(
+        condition=expr("coverage_availability == 'No'"),
+        set={"coverage_availability": expr("'Yes'")},
+    )
+
+    delta_table.delete(condition=expr("connectivity is NULL"))
+
+    return delta_table.toDF().show()
+
 
 if __name__ == "__main__":
-    from src.utils.spark import get_spark_session
-
-    # TO DO: Change file_url to a cloned one. DON'T USE ACTUAL GOLD DATA
-    file_url = f"{settings.AZURE_BLOB_CONNECTION_URI}/gold/delta-tables/BEN"
+    # CLONE TABLE
     spark = get_spark_session()
-    df = spark.read.format("delta").load(file_url)
-    df.select("coverage_availability").show()
-    df = df.withColumn(
-        "coverage_availability",
-        f.expr(
-            "CASE "
-            "   WHEN coverage_availability = 'Yes' "
-            "       THEN 'No' "
-            "   WHEN coverage_availability = 'No' "
-            "       THEN 'Yes' "
-            "ELSE coverage_availability END"
-        ),
-    )
-    df.select("coverage_availability").show()
-    # TO DO: Rewrite / send the modified file back to ADLS
+    v2_filepath = "gold/delta-tables-v2/BLZ"
+    v2_clone = "gold/delta-tables-v2/ZCDFTESTBLZ"
+    clone_table(spark=spark, filepath=v2_filepath, cloned_filepath=v2_clone)
+
+    # PRINT TABLE COLUMNS
+    delta_table = download_delta_table_as_delta_table(filepath=v2_clone, spark=spark)
+    delta_table.toDF().select("connectivity").show()
+    delta_table.toDF().select("coverage_availability").show()
+
+    # UPDATE TABLE
+    update_values(adls_path=v2_clone, spark=spark)
+
+    # PRINT UPDATED TABLE
+    delta_table = download_delta_table_as_delta_table(filepath=v2_clone, spark=spark)
+    delta_table.toDF().select("connectivity").show()
+    delta_table.toDF().select("coverage_availability").show()
