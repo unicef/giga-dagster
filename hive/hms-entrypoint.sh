fd858d4c18a2656f837e56303e933d1b6b8c8116
Kenneth V. Domingo <kenneth@thinkingmachin.es>
2024-02-13 23:57:53 +0800

    fix: errors on hms restart

--- hive/hms-entrypoint.sh
+++ hive/hms-entrypoint.sh
@@ -2,8 +2,6 @@
 
 set -euxo pipefail
 
-mkdir -p /hive_conf
-
 sed -e "s|{ENV:HMS_DATABASE_URL}|$HMS_DATABASE_URL|" \
     -e "s|{ENV:HMS_POSTGRESQL_USERNAME}|$HMS_POSTGRESQL_USERNAME|" \
     -e "s|{ENV:HMS_POSTGRESQL_PASSWORD}|$HMS_POSTGRESQL_PASSWORD|" \
@@ -16,4 +14,26 @@ sed -e "s|{ENV:METASTORE_WAREHOUSE_DIR}|$METASTORE_WAREHOUSE_DIR|" \
     -e "s|{ENV:HMS_POSTGRESQL_PASSWORD}|$HMS_POSTGRESQL_PASSWORD|" \
     /opt/hive/tpl/metastore-site.template.xml > /opt/hive/conf/metastore-site.xml
 
-exec sh -c /entrypoint.sh
+
+SKIP_SCHEMA_INIT="${IS_RESUME:-false}"
+
+export HIVE_CONF_DIR=$HIVE_HOME/conf
+
+if [ -d "${HIVE_CUSTOM_CONF_DIR:-}" ]; then
+  find "${HIVE_CUSTOM_CONF_DIR}" -type f -exec \
+    ln -sfn {} "${HIVE_CONF_DIR}"/ \;
+  export HADOOP_CONF_DIR=$HIVE_CONF_DIR
+  export TEZ_CONF_DIR=$HIVE_CONF_DIR
+fi
+
+export HADOOP_CLIENT_OPTS="${HADOOP_CLIENT_OPTS:-} -Xmx1G ${SERVICE_OPTS:-}"
+export METASTORE_PORT=${METASTORE_PORT:-9083}
+
+
+if $HIVE_HOME/bin/schematool --verbose -dbType postgres -validate | grep 'Done with metastore validation' | grep '[SUCCESS]'; then
+  echo 'Database OK'
+else
+  $HIVE_HOME/bin/schematool --verbose -dbType postgres -initSchema
+fi
+
+exec $HIVE_HOME/bin/hive --skiphadoopversion --skiphbasecp --service $SERVICE_NAME
