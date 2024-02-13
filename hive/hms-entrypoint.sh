019986f36a1ae4eccd4b1d9d3bfb1690bfb158f1
Kenneth V. Domingo <kenneth@thinkingmachin.es>
2024-02-13 23:34:12 +0800

    Revert "feat: [PARTIAL] use minimal hms setup"
    This reverts commit 619e7ad7aeebebce33f53e1fb9184295742bd000.

--- hive/hms-entrypoint.sh
+++ hive/hms-entrypoint.sh
@@ -2,22 +2,18 @@
 
 set -euxo pipefail
 
+mkdir -p /hive_conf
+
 sed -e "s|{ENV:HMS_DATABASE_URL}|$HMS_DATABASE_URL|" \
     -e "s|{ENV:HMS_POSTGRESQL_USERNAME}|$HMS_POSTGRESQL_USERNAME|" \
     -e "s|{ENV:HMS_POSTGRESQL_PASSWORD}|$HMS_POSTGRESQL_PASSWORD|" \
-    /opt/hive-metastore/tpl/hive-site.template.xml > /opt/hadoop/etc/hadoop/hive-site.xml
+    /opt/hive/tpl/hive-site.template.xml > /opt/hive/conf/hive-site.xml
 
 sed -e "s|{ENV:METASTORE_WAREHOUSE_DIR}|$METASTORE_WAREHOUSE_DIR|" \
     -e "s|{ENV:HMS_DATABASE_URL}|$HMS_DATABASE_URL|" \
     -e "s|{ENV:AZURE_SAS_TOKEN}|$AZURE_SAS_TOKEN|" \
     -e "s|{ENV:HMS_POSTGRESQL_USERNAME}|$HMS_POSTGRESQL_USERNAME|" \
     -e "s|{ENV:HMS_POSTGRESQL_PASSWORD}|$HMS_POSTGRESQL_PASSWORD|" \
-    /opt/hive-metastore/tpl/metastore-site.template.xml > /opt/hive-metastore/conf/metastore-site.xml
+    /opt/hive/tpl/metastore-site.template.xml > /opt/hive/conf/metastore-site.xml
 
-if /opt/hive-metastore/bin/schematool -dbType "$DATABASE_TYPE" -validate | grep 'Done with metastore validation' | grep '[SUCCESS]'; then
-  echo 'Database OK'
-else
-  /opt/hive-metastore/bin/schematool --verbose -dbType "$DATABASE_TYPE" -initSchema
-fi
-
-exec /opt/hive-metastore/bin/start-metastore
+exec sh -c /entrypoint.sh
