ac29982321c1d0c351a9fe2258da997ceed8a93b
tm-erin-cheng <erin@thinkingmachin.es>
2024-02-14 17:09:47 +0800

    fix: reset table names

--- dagster/src/resources/io_managers/adls_delta.py
+++ dagster/src/resources/io_managers/adls_delta.py
@@ -3,7 +3,6 @@ from pyspark import sql
 
 from dagster import InputContext, OutputContext
 from src.resources.io_managers.base import BaseConfigurableIOManager
-from src.settings import settings
 from src.utils.adls import ADLSFileClient
 
 adls_client = ADLSFileClient()
@@ -14,8 +13,7 @@ class ADLSDeltaIOManager(BaseConfigurableIOManager):
 
     def handle_output(self, context: OutputContext, output: sql.DataFrame):
         filepath = self._get_filepath(context)
-        # table_path = self._get_table_path(context, filepath)
-        table_path = f"{settings.AZURE_BLOB_CONNECTION_URI}/{'/'.join(filepath.split('/')[:-1])}/erinstable"
+        table_path = self._get_table_path(context, filepath)
 
         if context.step_key == "data_quality_results":
             adls_client.upload_json(filepath, output)
@@ -44,9 +42,7 @@ class ADLSDeltaIOManager(BaseConfigurableIOManager):
 
     def load_input(self, context: InputContext) -> sql.DataFrame:
         filepath = self._get_filepath(context.upstream_output)
-        # table_path = self._get_table_path(context.upstream_output, filepath)
-
-        table_path = f"{settings.AZURE_BLOB_CONNECTION_URI}/{'/'.join(filepath.split('/')[:-1])}/erinstable"
+        table_path = self._get_table_path(context.upstream_output, filepath)
 
         file = adls_client.download_delta_table_as_spark_dataframe(
             table_path, self.pyspark.spark_session
