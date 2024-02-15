240b0d9221d9f743135c05f336c1c336ccf7205b
tm-erin-cheng <erin@thinkingmachin.es>
2024-02-15 10:39:23 +0800

    feat: add new io manager

--- dagster/src/resources/io_managers/__init__.py
+++ dagster/src/resources/io_managers/__init__.py
@@ -1,7 +1,9 @@
 from .adls_delta import ADLSDeltaIOManager
+from .adls_json import ADLSJSONIOManager
 from .adls_pandas import ADLSPandasIOManager
 
 __all__ = [
-    "ADLSPandasIOManager",
     "ADLSDeltaIOManager",
+    "ADLSJSONIOManager",
+    "ADLSPandasIOManager",
 ]
