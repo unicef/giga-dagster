0ca99a614b1fceb541af5f392bebcde574d446e6
tm-erin-cheng <erin@thinkingmachin.es>
2024-02-15 10:38:38 +0800

    feat: split io manager for json

--- dagster/src/resources/io_managers/adls_json.py
+++ dagster/src/resources/io_managers/adls_json.py
@@ -2,49 +2,35 @@ from dagster_pyspark import PySparkResource
 from pyspark import sql
 
 from dagster import InputContext, OutputContext
-from src.resources.io_managers.base import BaseConfigurableIOManager
 from src.utils.adls import ADLSFileClient
+
+from .base import BaseConfigurableIOManager
 
 adls_client = ADLSFileClient()
 
 
-class ADLSDeltaIOManager(BaseConfigurableIOManager):
+class ADLSJSONIOManager(BaseConfigurableIOManager):
     pyspark: PySparkResource
 
     def handle_output(self, context: OutputContext, output: sql.DataFrame):
         filepath = self._get_filepath(context)
-        table_path = self._get_table_path(context, filepath)
-
         if output.isEmpty():
             context.log.warning("Output DataFrame is empty. Skipping write operation.")
             return
 
-        schema_name = self._get_schema(context)
-        type_transform_function = self._get_type_transform_function(context)
-        output = type_transform_function(output, context)
-        adls_client.upload_spark_dataframe_as_delta_table(
-            output,
-            table_path,
-            schema_name,
-            self.pyspark.spark_session,
-        )
+        adls_client.upload_json(output.toPandas().to_json(), filepath)
 
         context.log.info(
             f"Uploaded {filepath.split('/')[-1]} to"
             f" {'/'.join(filepath.split('/')[:-1])} in ADLS."
         )
 
-    def load_input(self, context: InputContext) -> sql.DataFrame:
+    def load_input(self, context: InputContext) -> list:
         filepath = self._get_filepath(context.upstream_output)
-        table_path = self._get_table_path(context.upstream_output, filepath)
-
-        data = adls_client.download_delta_table_as_spark_dataframe(
-            table_path, self.pyspark.spark_session
-        )
+        data = adls_client.download_json(filepath)
 
         context.log.info(
             f"Downloaded {filepath.split('/')[-1]} from"
             f" {'/'.join(filepath.split('/')[:-1])} in ADLS."
         )
-
         return data
