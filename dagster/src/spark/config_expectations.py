68502ca359d0fa0a711a6cf906a5dcc63a75cb39
Renz Togonon <renz.togonon@thinkingmachin.es>
2024-02-16 16:18:26 +0800

    feat: row level assertions

--- dagster/src/spark/config_expectations.py
+++ dagster/src/spark/config_expectations.py
@@ -2,89 +2,62 @@ import datetime
 
 SIMILARITY_RATIO_CUTOFF = 0.7
 
+# Single Column w/ parameters
+date_today = datetime.date.today()
+current_year = date_today.year
+
 # Single column checks
-CONFIG_UNIQUE_COLUMNS = ["school_id_gov", "school_id_giga"]
-CONFIG_NONEMPTY_COLUMNS_CRITICAL = [
+CONFIG_UNIQUE_COLUMNS_MASTER = ["school_id_govt", "school_id_giga"]
+
+CONFIG_UNIQUE_COLUMNS_REFERENCE = ["school_id_giga"]
+
+CONFIG_UNIQUE_COLUMNS_GEOLOCATION = ["school_id_govt", "school_id_giga"]
+
+CONFIG_UNIQUE_COLUMNS_COVERAGE = ["school_id_giga"]
+
+CONFIG_UNIQUE_COLUMNS_CRITICAL = ["school_id_govt", "school_id_giga"]
+
+CONFIG_NONEMPTY_COLUMNS_MASTER = [
+    "school_id_giga",
+    "school_id_govt",
     "school_name",
     "longitude",
     "latitude",
     "education_level",
 ]
-CONFIG_NONEMPTY_COLUMNS_WARNING = [
-    "mobile_internet_generation",
-    "internet_availability",
-    "internet_type",
-    "download_speed_govt",
+
+CONFIG_NONEMPTY_COLUMNS_REFERENCE = [
+    "school_id_giga",
+    "education_level_govt",
+    "school_id_govt_type",
 ]
 
-CONFIG_NOT_SIMILAR_COLUMN = ["school_name"]
-CONFIG_FIVE_DECIMAL_PLACES = ["latitude", "longitude"]
-
-# Multi-column checks
-CONFIG_UNIQUE_SET_COLUMNS = [
-    ["school_id_gov", "school_name", "education_level", "location_id"],
-    ["school_name", "education_level", "location_id"],
-    ["education_level", "location_id"],
-    # everything except school code
-    # school name educ lat 110 lon 110
-    # similar school name educ lat 110 lon 110
+CONFIG_NONEMPTY_COLUMNS_GEOLOCATION = [
+    "school_id_giga",
+    "school_id_govt",
+    "school_name",
+    "longitude",
+    "latitude",
+    "education_level",
+    "education_level_govt",
+    "school_id_govt_type"
 ]
 
-CONFIG_COLUMN_SUM = [
-    ["student_count_girls", "student_count_boys", "student_count_others"]
+CONFIG_NONEMPTY_COLUMNS_COVERAGE = [
+    "school_id_giga",
 ]
 
-# Single Column w/ parameters
-date_today = datetime.date.today()
-current_year = date_today.year
+CONFIG_NONEMPTY_COLUMNS_CRITICAL = [
+    "school_name",
+    "longitude",
+    "latitude",
+]
 
-CONFIG_VALUES_RANGE_PRIO = {
-    "download_speed_govt": {"min": 1, "max": 200},
-    "school_density": {"min": 0, "max": 5},
-}
 
-# For GOVERNMENT SCHOOL COLUMNS
-CONFIG_VALUES_RANGE = {
-    # Mandatory
-    "download_speed_govt": {"min": 1, "max": 200},
-    "download_speed_contracted": {"min": 1, "max": 500},
-    "num_students": {"min": 20, "max": 2500},
-    "latitude": {"min": -90, "max": 90},
-    "longitude": {"min": -180, "max": 180},
-    "school_establishment_year": {"min": 1000, "max": current_year},
-    # # Calculated
-    "school_density": {"min": 0, "max": 5},
-    # Optional
-    "download_speed_govt": {"min": 0, "max": 200},
-    "download_speed_govt_contracted": {"min": 0, "max": 500},
-    # "connectivity_latency_static": {"min": 0, "max": 200},
-    "num_computers": {"min": 0, "max": 500},
-    "num_computers_desired": {"min": 0, "max": 1000},
-    "num_teachers": {"min": 0, "max": 200},
-    "num_adm_personnel": {"min": 0, "max": 200},
-    "num_students": {"min": 0, "max": 10000},
-    "num_classroom": {"min": 0, "max": 200},
-    "num_latrines": {"min": 0, "max": 200},
-    "school_data_collection_year": {"min": 1000, "max": current_year},
-}
-
-CONFIG_VALUES_OPTIONS = {
-    "computer_lab": ["yes", "no"],
-    "electricity_availability": ["yes", "no"],
-    "water_availability": ["yes", "no"],
-    "cellular_network_availability": ["yes", "no"],
+CONFIG_VALUES_DOMAIN_MASTER = {
+    "cellular_coverage_availability": ["yes", "no", None],
+    "cellular_coverage_type": ["2G", "3G", "4G", "5G", "no coverage"],
     "connectivity_govt": ["yes", "no"],
-    "school_is_open": ["yes", "no"],
-    "school_daily_check_app": ["yes", "no"],
-    "2G_coverage": ["true", "false"],
-    "3G_coverage": ["true", "false"],
-    "4G_coverage": ["true", "false"],
-    "education_level_isced": [
-        "childhood education",
-        "primary education",
-        "secondary education",
-        "post secondary education",
-    ],
     "education_level": [
         "Pre-Primary",
         "Primary",
@@ -93,8 +66,9 @@ CONFIG_VALUES_OPTIONS = {
         "Pre-Primary and Primary",
         "Primary and Secondary",
         "Pre-Primary, Primary and Secondary",
+        "Primary, Secondary and Post-Secondary",
     ],
-    "connectivity_type": [
+    "connectivity_type_govt": [
         "fiber",
         "xdsl",
         "wired",
@@ -105,34 +79,173 @@ CONFIG_VALUES_OPTIONS = {
         "other",
     ],
     "school_area_type": ["urban", "rural"],
-    "school_type_public": ["public", "not public"],
+    "computer_lab": ["yes", "no"],
+    "electricity_availability": ["yes", "no"],
     "electricity_type": [
         "electrical grid",
         "diesel generator",
         "solar power station",
         "other",
     ],
-    "school_data_collection_modality": ["online", "in-person", "phone", "other"],
+    "water_availability": ["yes", "no"],
+    "school_data_collection_modality": [
+        "online", 
+        "in-person", 
+        "phone", 
+        "other"
+    ],
+}
+
+CONFIG_VALUES_DOMAIN_REFERENCE = {
+    "is_school_open": ["yes", "no"],
+}
+CONFIG_VALUES_DOMAIN_GEOLOCATION = {
+    "connectivity_govt": ["yes", "no"],
+    "education_level": [
+        "Pre-Primary",
+        "Primary",
+        "Secondary",
+        "Post-Secondary",
+        "Pre-Primary and Primary",
+        "Primary and Secondary",
+        "Pre-Primary, Primary and Secondary",
+    ],
+    "connectivity_type_govt": [
+        "fiber",
+        "xdsl",
+        "wired",
+        "cellular",
+        "p2mp wireless",
+        "p2p wireless",
+        "satellite",
+        "other",
+    ],
+    "school_area_type": ["urban", "rural"],
+    "computer_lab": ["yes", "no"],
+    "electricity_availability": ["yes", "no"],
+    "electricity_type": [
+        "electrical grid",
+        "diesel generator",
+        "solar power station",
+        "other",
+    ],
+    "water_availability": ["yes", "no"],
+    "school_data_collection_modality": [
+        "online", 
+        "in-person", 
+        "phone", 
+        "other"
+    ],
+
+
+    # "cellular_network_availability": ["yes", "no"],
+    # "2G_coverage": ["true", "false"],
+    # "3G_coverage": ["true", "false"],
+    # "4G_coverage": ["true", "false"],
+    # # "education_level_isced": [
+    #     "childhood education",
+    #     "primary education",
+    #     "secondary education",
+    #     "post secondary education",
+    # ],
+    # "school_type_public": ["public", "not public"],
     
 }
 
-CONFIG_VALUES_OPTIONS_COVERAGE = {
+CONFIG_VALUES_DOMAIN_COVERAGE = {
     "cellular_coverage_availability": ["yes", "no", None],
-    "cellular_coverage_type": ["2g", "3g", "4g", "5g"],
+    "cellular_coverage_type": ["2G", "3G", "4G", "5G", "no coverage"],
 }
 
+
 # For COVERAGE ITU dataset. To separate COVERAGE ITU expectations into different file.
-CONFIG_VALUES_RANGE_COVERAGE_ITU = {
-    "fiber_node_distance": {"min": 0, "max": None},
-    "microwave_node_distance": {"min": 0, "max": None},
+CONFIG_VALUES_RANGE_COVERAGE = {
+    "fiber_node_distance": {"min": 0, "max": 10000000},
+    "microwave_node_distance": {"min": 0, "max": 10000000},
     "schools_within_1km": {"min": 0, "max": 20},
     "schools_within_2km": {"min": 0, "max": 40},
     "schools_within_3km": {"min": 0, "max": 60},
-    "schools_within_10km": {"min": 0, "max": 100},
     "nearest_school_distance": {"min": 0, "max": None},
+    "schools_within_10km": {"min": 0, "max": 100},
 }
 
-CONFIG_VALUES_TYPE = [{"column": "school_id_gov", "type": "int64"}]
+CONFIG_VALUES_RANGE_REFERENCE = {
+    "nearest_school_distance": {"min": 0, "max": None},
+    "schools_within_10km": {"min": 0, "max": 100},
+    "download_speed_govt": {"min": 1, "max": 200},
+}
+
+CONFIG_VALUES_RANGE_MASTER = {
+    "fiber_node_distance": {"min": 0, "max": 10000000},
+    "microwave_node_distance": {"min": 0, "max": 10000000},
+    "schools_within_1km": {"min": 0, "max": 20},
+    "schools_within_2km": {"min": 0, "max": 40},
+    "schools_within_3km": {"min": 0, "max": 60},
+    "school_establishment_year": {"min": 1000, "max": current_year},
+    "latitude": {"min": -90, "max": 90},
+    "longitude": {"min": -180, "max": 180},
+    "download_speed_contracted": {"min": 1, "max": 500},
+    "num_computers": {"min": 0, "max": 500},
+    "num_computers_desired": {"min": 0, "max": 1000},
+    "num_teachers": {"min": 0, "max": 200},
+    "num_adm_personnel": {"min": 0, "max": 200},
+    "num_students": {"min": 0, "max": 10000},
+    "num_classroom": {"min": 0, "max": 200},
+    "num_latrines": {"min": 0, "max": 200},
+    "school_data_collection_year": {"min": 1000, "max": current_year},
+}
+
+CONFIG_VALUES_RANGE_GEOLOCATION = {
+    "school_establishment_year": {"min": 1000, "max": current_year},
+    "latitude": {"min": -90, "max": 90},
+    "longitude": {"min": -180, "max": 180},
+    "download_speed_contracted": {"min": 1, "max": 500},
+    "num_computers": {"min": 0, "max": 500},
+    "num_computers_desired": {"min": 0, "max": 1000},
+    "num_teachers": {"min": 0, "max": 200},
+    "num_adm_personnel": {"min": 0, "max": 200},
+    "num_students": {"min": 0, "max": 10000},
+    "num_classroom": {"min": 0, "max": 200},
+    "num_latrines": {"min": 0, "max": 200},
+    "school_data_collection_year": {"min": 1000, "max": current_year},
+    "download_speed_govt": {"min": 1, "max": 200},
+}
+
+CONFIG_VALUES_RANGE_CRITICAL = {
+    "latitude": {"min": -90, "max": 90},
+    "longitude": {"min": -180, "max": 180},
+}
+
+
+CONFIG_UNIQUE_SET_COLUMNS = [
+    ["school_id_govt", "school_name", "education_level", "location_id"],
+    ["school_name", "education_level", "location_id"],
+    ["education_level", "location_id"],
+    # school name educ lat 110 lon 110
+    # similar school name educ lat 110 lon 110
+]
+
+CONFIG_NOT_SIMILAR_COLUMN = ["school_name"]
+CONFIG_FIVE_DECIMAL_PLACES = ["latitude", "longitude"]
+
+# Multi-column checks
+
+CONFIG_COLUMN_SUM = [
+    ["student_count_girls", "student_count_boys", "student_count_others"]
+]
+
+
+CONFIG_VALUES_RANGE_PRIO = {
+    "download_speed_govt": {"min": 1, "max": 200},
+    # "school_density": {"min": 0, "max": 5},
+}
+
+# For GOVERNMENT SCHOOL COLUMNS
+
+
+
+
+CONFIG_VALUES_TYPE = [{"column": "school_id_govt", "type": "int64"}]
 
 # Column Pairs
 CONFIG_PAIR_AVAILABILITY = [
@@ -192,3 +305,108 @@ CONFIG_COV_COLUMN_RENAME = {
     }
 
 CONFIG_COV_COLUMN_MERGE_LOGIC = ["cellular_coverage_availability", "cellular_coverage_type"]
+
+CONFIG_COLUMNS_EXCEPT_SCHOOL_ID_GEOLOCATION =[
+    [
+    # "school_id_giga",
+    # "school_id_govt",
+    "school_name",
+    "school_establishment_year",
+    "latitude",
+    "longitude",
+    "education_level",
+    "education_level_govt",
+    "connectivity_govt",
+    "connectivity_govt_ingestion_timestamp",
+    "connectivity_govt_collection_year",
+    "download_speed_govt",
+    "download_speed_contracted",
+    "connectivity_type_govt",
+    "admin1",
+    "admin2",
+    "school_area_type",
+    "school_funding_type",
+    "num_computers",
+    "num_computers_desired",
+    "num_teachers",
+    "num_adm_personnel",
+    "num_students",
+    "num_classroom",
+    "num_latrines",
+    "computer_lab",
+    "electricity_availability",
+    "electricity_type",
+    "water_availability",
+    "school_data_source",
+    "school_data_collection_year",
+    "school_data_collection_modality",
+    "school_id_govt_type",
+    "school_address",
+    "is_school_open",
+    "school_location_ingestion_timestamp",  
+    ]
+]
+
+CONFIG_COLUMNS_EXCEPT_SCHOOL_ID_MASTER =[
+    [
+    "cellular_coverage_availability",
+    "cellular_coverage_type",
+    "fiber_node_distance",
+    "microwave_node_distance",
+    "schools_within_1km",
+    "schools_within_2km",
+    "schools_within_3km",
+    "nearest_LTE_distance",
+    "nearest_UMTS_distance",
+    "nearest_GSM_distance",
+    "pop_within_1km",
+    "pop_within_2km",
+    "pop_within_3km",
+    "connectivity_govt_collection_year",
+    "connectivity_govt",
+    # "school_id_giga",
+    # "school_id_govt",
+    "school_name",
+    "school_establishment_year",
+    "latitude",
+    "longitude",
+    "education_level",
+    "download_speed_contracted",
+    "connectivity_type_govt",
+    "admin1",
+    # "admin1_id_giga",
+    "admin2",
+    # "admin2_id_giga",
+    "school_area_type",
+    "school_funding_type",
+    "num_computers",
+    "num_computers_desired",
+    "num_teachers",
+    "num_adm_personnel",
+    "num_students",
+    "num_classroom",
+    "num_latrines",
+    "computer_lab",
+    "electricity_availability",
+    "electricity_type",
+    "water_availability",
+    "school_data_source",
+    "school_data_collection_year",
+    "school_data_collection_modality",
+    "connectivity_govt_ingestion_timestamp",
+    "school_location_ingestion_timestamp",
+    "disputed_region",
+    "connectivity",
+    "connectivity_RT",
+    "connectivity_RT_datasource",
+    "connectivity_RT_ingestion_timestamp", 
+    ]
+]
+
+
+#notes 
+# admin 1 admin 2 connectivity_RT nonnullable
+# admin 1 admin 2 connectivity_RT required
+# valid lat lon = decimal check and range check
+# added country grouping in checking for duplicates because appended all countries in one dataset
+# added educational level "Primary, Secondary and Post-Secondary"
