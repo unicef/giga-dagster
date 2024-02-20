93d8c36e0002514ef5722883a80c67c962c9e5d0
tm-erin-cheng <124675099+tm-erin-cheng@users.noreply.github.com>
2024-02-20 11:48:12 +0800

    Feat/coverage pipelines (#41)
    * chore: minor cleanup
    
    * chore: renamed files, add bronze and silver table sql
    
    * chore: remove adls_bronze, remove transform functions for delta table in gold
    
    * chore: remove adls_bronze
    
    * chore: remove unused imports
    
    * feat: add new methods for staging tables to adls client
    
    * feat: add temporary staging table - merge of silver and dq_passed
    
    * feat: add a way for staging table to handle multiple raw uploads for the same country
    
    * feat: change delta table name for non gold/silver tables
    
    * chore: change how files are downloaded
    
    * clean: remove unused methods
    
    * chore: pop stashed changes
    
    * chore: save wip
    
    * feat: add schema for layers
    
    * fix: update poetry
    
    * fix: fix folders
    
    * fix: comment out spark config
    
    * chore: add note to self
    
    * refactor: delete files
    
    * refactor: delete gx resource
    
    * refactor: delete files
    
    * refactor: remove gx from assets and transforms
    
    * refactor: delete gx from configs
    
    * refactor: remove gx from poetry lock
    
    * refactor: remove gx from assets
    
    * refactor: delete gx from poetry
    
    * chore: rename folders
    
    * feat: add coverage pipeline
    
    * refactor: move common assets to a folder
    
    * refactor: fix jobs for coverage
    
    * refactor: simplify
    
    * feat: add coverage sensors
    
    * feat: add comments re: flow
    
    * chore: updated
    
    * fix: add coverage assets and sensors
    
    * fix: redo schema files
    
    * feat: add sensors and new paths for coverage data
    
    * feat: fix io manager configs
    
    * feat: fix pipelines for geolocation and coverage
    
    * chore: lint
    
    * fix: rename schemas for consistency
    
    * fix: refactor io manager, reduce args
    
    * fix: formatting, add coverage paths to folder mapping
    
    * feat: staging flow
    
    * fix: fixes, lints
    
    * chore: debug
    
    * fix: geolocation staging step
    
    * fix: clean up logging, fix folder paths
    
    * feat: add configs for coverage
    
    * feat: fix type casting functions
    
    * fix: reset table names
    
    * fix: folder path
    
    * fix: lints
    
    * fix: rename io manager
    
    * feat: add multiasset for results
    
    * feat: renamed io manager
    
    * fix: removed spark io manager
    
    * fix: clean comments
    
    * fix: rename io manager
    
    * fix: rename io manager
    
    * feat: split io manager for json
    
    * feat: add new io manager
    
    * fix: remove pyspark from io manager
    
    * fix: fix asset configs
    
    * feat: add check for existing stg table before cloning silver
    
    * feat: add better staging logic
    
    * fix: reorder
    
    ---------
    
    Co-authored-by: Kenneth V. Domingo <kenneth@thinkingmachin.es>

--- dagster/src/definitions.py
+++ dagster/src/definitions.py
@@ -1,28 +1,34 @@
 from dagster import Definitions, load_assets_from_package_module
-from src.assets import assets, datahub_assets, qos
+from src.assets import common, datahub_assets, qos, school_coverage, school_geolocation
 from src.jobs import (
     datahub__create_domains_job,
     datahub__create_tags_job,
     datahub__ingest_azure_ad_users_groups_job,
     datahub__update_policies_job,
     qos__convert_csv_to_deltatable_job,
-    school_master__automated_data_checks_job,
     school_master__convert_gold_csv_to_deltatable_job,
-    school_master__failed_manual_checks_job,
-    school_master__successful_manual_checks_job,
+    school_master_coverage__automated_data_checks_job,
+    school_master_coverage__failed_manual_checks_job,
+    school_master_coverage__successful_manual_checks_job,
+    school_master_geolocation__automated_data_checks_job,
+    school_master_geolocation__failed_manual_checks_job,
+    school_master_geolocation__successful_manual_checks_job,
     school_reference__convert_gold_csv_to_deltatable_job,
 )
 from src.resources.io_managers import (
-    ADLSBronzeIOManager,
     ADLSDeltaIOManager,
-    ADLSRawIOManager,
+    ADLSJSONIOManager,
+    ADLSPandasIOManager,
 )
 from src.sensors import (
     qos__csv_to_deltatable_sensor,
-    school_master__failed_manual_checks_sensor,
     school_master__gold_csv_to_deltatable_sensor,
-    school_master__raw_file_uploads_sensor,
-    school_master__successful_manual_checks_sensor,
+    school_master_coverage__failed_manual_checks_sensor,
+    school_master_coverage__raw_file_uploads_sensor,
+    school_master_coverage__successful_manual_checks_sensor,
+    school_master_geolocation__failed_manual_checks_sensor,
+    school_master_geolocation__raw_file_uploads_sensor,
+    school_master_geolocation__successful_manual_checks_sensor,
     school_reference__gold_csv_to_deltatable_sensor,
 )
 from src.utils.adls import ADLSFileClient
@@ -35,24 +41,31 @@ setup_sentry()
 defs = Definitions(
     assets=[
         *load_assets_from_package_module(
-            package_module=assets, group_name="school_master_data"
+            package_module=school_geolocation, group_name="school_geolocation_data"
         ),
+        *load_assets_from_package_module(
+            package_module=school_coverage, group_name="school_coverage_data"
+        ),
+        *load_assets_from_package_module(package_module=common, group_name="common"),
         *load_assets_from_package_module(package_module=qos, group_name="qos_data"),
         *load_assets_from_package_module(
             package_module=datahub_assets, group_name="datahub"
         ),
     ],
     resources={
-        "adls_raw_io_manager": ADLSRawIOManager(pyspark=pyspark),
-        "adls_bronze_io_manager": ADLSBronzeIOManager(pyspark=pyspark),
         "adls_delta_io_manager": ADLSDeltaIOManager(pyspark=pyspark),
+        "adls_json_io_manager": ADLSJSONIOManager(),
+        "adls_pandas_io_manager": ADLSPandasIOManager(),
         "adls_file_client": ADLSFileClient(),
         "spark": pyspark,
     },
     jobs=[
-        school_master__automated_data_checks_job,
-        school_master__successful_manual_checks_job,
-        school_master__failed_manual_checks_job,
+        school_master_coverage__automated_data_checks_job,
+        school_master_coverage__failed_manual_checks_job,
+        school_master_coverage__successful_manual_checks_job,
+        school_master_geolocation__automated_data_checks_job,
+        school_master_geolocation__failed_manual_checks_job,
+        school_master_geolocation__successful_manual_checks_job,
         school_master__convert_gold_csv_to_deltatable_job,
         school_reference__convert_gold_csv_to_deltatable_job,
         qos__convert_csv_to_deltatable_job,
@@ -62,11 +75,14 @@ defs = Definitions(
         datahub__update_policies_job,
     ],
     sensors=[
-        school_master__raw_file_uploads_sensor,
-        school_master__successful_manual_checks_sensor,
-        school_master__failed_manual_checks_sensor,
+        qos__csv_to_deltatable_sensor,
         school_master__gold_csv_to_deltatable_sensor,
         school_reference__gold_csv_to_deltatable_sensor,
-        qos__csv_to_deltatable_sensor,
+        school_master_geolocation__raw_file_uploads_sensor,
+        school_master_coverage__raw_file_uploads_sensor,
+        school_master_geolocation__successful_manual_checks_sensor,
+        school_master_coverage__successful_manual_checks_sensor,
+        school_master_geolocation__failed_manual_checks_sensor,
+        school_master_coverage__failed_manual_checks_sensor,
     ],
 )
--- dagster/src/resources/io_managers/__init__.py
+++ dagster/src/resources/io_managers/__init__.py
@@ -1,9 +1,9 @@
-from .adls_bronze import ADLSBronzeIOManager
 from .adls_delta import ADLSDeltaIOManager
-from .adls_raw import ADLSRawIOManager
+from .adls_json import ADLSJSONIOManager
+from .adls_pandas import ADLSPandasIOManager
 
 __all__ = [
-    "ADLSRawIOManager",
-    "ADLSBronzeIOManager",
     "ADLSDeltaIOManager",
+    "ADLSJSONIOManager",
+    "ADLSPandasIOManager",
 ]
