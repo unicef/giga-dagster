565aadb92fc5246f276c7397584ecb5263c62e41
tm-erin-cheng <erin@thinkingmachin.es>
2024-02-20 16:46:36 +0800

    fix: temp comment out multi asset

--- dagster/src/sensors.py
+++ dagster/src/sensors.py
@@ -88,9 +88,9 @@ def school_master_geolocation__raw_file_uploads_sensor():
                         "geolocation_bronze": get_file_config(
                             "geolocation_bronze", file_config_params
                         ),
-                        "geolocation_data_quality_results": get_file_config(
-                            "geolocation_data_quality_results", file_config_params
-                        ),
+                        # "geolocation_data_quality_results": get_file_config(
+                        #     "geolocation_data_quality_results", file_config_params
+                        # ),
                         "geolocation_dq_passed_rows": get_file_config(
                             "geolocation_dq_passed_rows", file_config_params
                         ),
@@ -152,9 +152,9 @@ def school_master_coverage__raw_file_uploads_sensor():
                         "coverage_raw": get_file_config(
                             "coverage_raw", file_config_params
                         ),
-                        "coverage_data_quality_results": get_file_config(
-                            "coverage_data_quality_results", file_config_params
-                        ),
+                        # "coverage_data_quality_results": get_file_config(
+                        #     "coverage_data_quality_results", file_config_params
+                        # ),
                         "coverage_dq_passed_rows": get_file_config(
                             "coverage_dq_passed_rows", file_config_params
                         ),
