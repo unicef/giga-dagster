de910189c1ad64f13acb69ab2794b6f9de20746e
tm-sofia-pineda <sofia.pineda@thinkingmachin.es>
2024-02-28 20:18:52 +0800

    feat: create json input for email report

--- dagster/src/utils/send_email_dq_report.py
+++ dagster/src/utils/send_email_dq_report.py
@@ -1,24 +1,22 @@
 import requests
 from loguru import logger
-from pyspark import sql
 from requests import HTTPError, JSONDecodeError
 
 from azure.communication.email import EmailClient
 from src.settings import settings
+from src.utils.adls import ADLSFileClient
 
 
-def send_email_dq_report(dq_report: sql.DataFrame):
+def send_email_dq_report(json):
     client = EmailClient.from_connection_string(settings.AZURE_EMAIL_CONNECTION_STRING)
 
     res = requests.post(
-        "http://email-api:3020/email/dq-report",
+        "http://localhost:3020/email/dq-report",
         headers={
             "Content-Type": "application/json",
             "Authorization": f"Bearer {settings.EMAIL_RENDERER_BEARER_TOKEN}",
         },
-        json={
-            "name": "John Doe",
-        },
+        json=json,
     )
     if not res.ok:
         try:
@@ -44,3 +42,18 @@ def send_email_dq_report(dq_report: sql.DataFrame):
     poller = client.begin_send(message)
     result = poller.result()
     logger.info(result)
+
+
+if __name__ == "__main__":
+    adls = ADLSFileClient()
+    dq_results_filepath = "logs-gx/test.json"
+    dq_results_json = adls.download_json(dq_results_filepath)
+
+    dataset_filepath = (
+        "adls-testing-raw/school-coverage-data/BIH_school-coverage_gov_20240227.csv"
+    )
+    properties = adls.get_file_metadata(filepath=dataset_filepath)
+    metadata = properties["metadata"]
+
+    json_input = metadata | dq_results_json
+    send_email_dq_report(json=json_input)
