ba8804dc028e9354e35ae09bcb58eb5ed46b5e11
Renz Togonon <renz.togonon@thinkingmachin.es>
2024-03-05 13:29:53 +0800

    fix: json report to consider non column level aggregations

--- dagster/src/data_quality_checks/utils.py
+++ dagster/src/data_quality_checks/utils.py
@@ -165,6 +165,7 @@ def aggregate_report_spark_df(
         "percent_failed",
         "percent_passed",
     )
+    report = report.withColumn("column", f.coalesce(f.col("column"), f.lit("")))
 
     return report
 
@@ -303,16 +304,16 @@ if __name__ == "__main__":
     # file_url = f"{settings.AZURE_BLOB_CONNECTION_URI}/adls-testing-raw/_test_BLZ_RAW.csv"
     df_bronze = spark.read.csv(file_url, header=True)
     print(df_bronze.count())
-    # df_bronze = df_bronze.sort("school_name").limit(1000)
+    df_bronze = df_bronze.sort("school_name").limit(100)
     df_bronze = df_bronze.withColumnRenamed("school_id_gov", "school_id_govt")
     df_bronze = df_bronze.withColumnRenamed("num_classroom", "num_classrooms")
     # df = domain_checks(df_bronze, VALUES_DOMAIN_MASTER)
     # df_test = has_similar_name(df_bronze)
 
-    df_duplicates, df_deduplicated = extract_school_id_govt_duplicates(df_bronze)
+    # df_duplicates, df_deduplicated = extract_school_id_govt_duplicates(df_bronze)
 
-    df_duplicates.where(df_duplicates.school_id_govt == '11514002').show()
-    df_deduplicated.where(df_deduplicated.school_id_govt == '11514002').show()
+    # df_duplicates.where(df_duplicates.school_id_govt == '11514002').show()
+    # df_deduplicated.where(df_deduplicated.school_id_govt == '11514002').show()
 
     
     # df_bronze.groupBy("school_id_govt").agg(f.count("*").alias("count")).orderBy("count", ascending=False).show()
@@ -326,17 +327,17 @@ if __name__ == "__main__":
     # df_bronze = df_bronze.withColumn("test", f.lower(f.col("admin2_id_giga")))
 
     # row_level_checks(df, dataset_type, country_code_iso3)
-    # df = row_level_checks(
-    #     df_bronze, "master", "GHA")
+    df = row_level_checks(
+        df_bronze, "master", "GIN")
     # dataset plugged in should conform to updated schema! rename if necessary
-    # df.show()
-    # df = df.withColumn("dq_has_critical_error", f.lit(1))
+    df.show()
+    df = df.withColumn("dq_has_critical_error", f.lit(1))
 
     # df = dq_passed_rows(df, "coverage")
     # df = dq_passed_rows(df, "coverage")
-    # df = aggregate_report_sparkdf(df)
-    # df.show()
+    df = aggregate_report_spark_df(spark=spark, df=df)
+    df.orderBy("column").show()
     # df.show()
 
-    # _json = aggregate_report_json(df, df_bronze)
-    # print(_json)
+    _json = aggregate_report_json(df, df_bronze)
+    print(_json)
