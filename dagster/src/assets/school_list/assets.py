67ba59f01e1c2f1fbbd163d30ba8120ee2335cec
tm-erin-cheng <erin@thinkingmachin.es>
2024-03-08 21:38:16 +0900

    feat: updated qos school list flow

--- dagster/src/assets/school_list/assets.py
+++ dagster/src/assets/school_list/assets.py
@@ -4,8 +4,19 @@ import pandas as pd
 from dagster_pyspark import PySparkResource
 from delta.tables import DeltaTable
 from pyspark import sql
+from src.data_quality_checks.utils import (
+    aggregate_report_json,
+    aggregate_report_spark_df,
+    row_level_checks,
+)
+from src.sensors.base import FileConfig
 from src.settings import settings
-from src.utils.adls import ADLSFileClient, get_filepath, get_output_filepath
+from src.spark.transform_functions import create_bronze_layer_columns
+from src.utils.adls import (
+    ADLSFileClient,
+    get_filepath,
+    get_output_filepath,
+)
 from src.utils.datahub.emit_dataset_metadata import emit_metadata_to_datahub
 
 from dagster import AssetOut, OpExecutionContext, Output, asset, multi_asset
@@ -14,23 +25,23 @@ from dagster import AssetOut, OpExecutionContext, Output, asset, multi_asset
 @asset(io_manager_key="adls_pandas_io_manager")
 def list_raw(
     context: OpExecutionContext,
+    adls_file_client: ADLSFileClient,
 ) -> pd.DataFrame:
-    # api pull
-    df = pd.DataFrame()
-    # IN: pandas df, OUT: csv file
+    df = adls_file_client.download_csv_as_pandas_dataframe(
+        context.run_tags["dagster/run_key"]
+    )
     emit_metadata_to_datahub(context, df=df)
     yield Output(df, metadata={"filepath": context.run_tags["dagster/run_key"]})
 
 
 @asset(io_manager_key="adls_pandas_io_manager")  # this is wrong
-def list_bronze(context: OpExecutionContext, list_raw: sql.DataFrame) -> pd.DataFrame:
-    # IN: spark datafame, OUT: csv file
-    # Transform columns added here, all column renaming done here
-    # Output should be stored as a spark dataframe
+def list_bronze(
+    context: OpExecutionContext,
+    list_raw: sql.DataFrame,
+) -> pd.DataFrame:
+    df = create_bronze_layer_columns(list_raw)
     emit_metadata_to_datahub(context, df=list_raw)
-    yield Output(
-        list_raw.toPandas(), metadata={"filepath": get_output_filepath(context)}
-    )
+    yield Output(df.toPandas(), metadata={"filepath": get_output_filepath(context)})
 
 
 @multi_asset(
@@ -39,73 +50,74 @@ def list_bronze(context: OpExecutionContext, list_raw: sql.DataFrame) -> pd.Data
             is_required=True, io_manager_key="adls_pandas_io_manager"
         ),
         "list_dq_summary_statistics": AssetOut(
-            is_required=True, io_manager_key="adls_pandas_io_manager"
-        ),
-        "list_dq_checks": AssetOut(
             is_required=True, io_manager_key="adls_json_io_manager"
         ),
     }
 )
 def list_data_quality_results(
     context,
+    config: FileConfig,
     list_bronze: sql.DataFrame,
+    spark: PySparkResource,
 ):
-    # Output is a spark dataframe (bronze + extra columns with dq results)
-    # Output is a spark dataframe(?) with summary statistics (for Ger)
-    # Output is a JSON with a list of checks (no results - Ger asked for)
+    country_code = context.run_tags["dagster/run_key"].split("/")[-1].split("_")[1]
+    dq_results = row_level_checks(list_bronze, "geolocation", country_code)
+    dq_summary_statistics = aggregate_report_json(
+        aggregate_report_spark_df(spark.spark_session, dq_results), list_bronze
+    )
+
     yield Output(
-        list_bronze.toPandas(),
-        metadata={"filepath": get_output_filepath(context)},
+        dq_results.toPandas(),
+        metadata={"filepath": get_output_filepath(context, "list_dq_results")},
         output_name="list_dq_results",
     )
 
     yield Output(
-        list_bronze.toPandas(),
-        metadata={"filepath": get_output_filepath(context)},
+        dq_summary_statistics,
+        metadata={
+            "filepath": get_output_filepath(context, "list_dq_summary_statistics")
+        },
         output_name="list_dq_summary_statistics",
     )
 
+
+@asset(io_manager_key="adls_pandas_io_manager")
+def list_dq_passed_rows(
+    context: OpExecutionContext,
+    list_dq_results: sql.DataFrame,
+) -> sql.DataFrame:
+    df_passed = list_dq_results
+    emit_metadata_to_datahub(context, df_passed)
     yield Output(
-        list_bronze,
-        metadata={"filepath": get_output_filepath(context)},
-        output_name="list_dq_checks",
+        df_passed.toPandas(), metadata={"filepath": get_output_filepath(context)}
     )
 
 
-@asset(io_manager_key="adls_delta_io_manager")
-def list_dq_passed_rows(
-    context: OpExecutionContext,
-    list_data_quality_results: sql.DataFrame,
-) -> sql.DataFrame:
-    df_passed = list_data_quality_results
-    df_passed.toPandas().loc[3, "school_id_giga"] = "ABCDEFGHIJKLM"
-    context.log.info(f"df_passed: {df_passed}")
-    yield Output(df_passed, metadata={"filepath": get_output_filepath(context)})
-
-
-@asset(io_manager_key="adls_delta_io_manager")
+@asset(io_manager_key="adls_pandas_io_manager")
 def list_dq_failed_rows(
     context: OpExecutionContext,
-    list_data_quality_results: sql.DataFrame,
+    list_dq_results: sql.DataFrame,
 ) -> sql.DataFrame:
-    df_failed = list_data_quality_results
+    df_failed = list_dq_results
     emit_metadata_to_datahub(context, df_failed)
-    yield Output(df_failed, metadata={"filepath": get_output_filepath(context)})
+    yield Output(
+        df_failed.toPandas(), metadata={"filepath": get_output_filepath(context)}
+    )
 
 
 @asset(io_manager_key="adls_delta_io_manager")
 def list_staging(
     context: OpExecutionContext,
-    geolocation_dq_passed_rows: sql.DataFrame,
+    list_dq_passed_rows: sql.DataFrame,
     adls_file_client: ADLSFileClient,
     spark: PySparkResource,
-) -> sql.DataFrame:
+):
     dataset_type = context.get_step_execution_context().op_config["dataset_type"]
     filepath = context.run_tags["dagster/run_key"]
     silver_table_path = f"{settings.AZURE_BLOB_CONNECTION_URI}/{get_filepath(filepath, dataset_type, 'silver').split('_')[0]}"
     staging_table_path = f"{settings.AZURE_BLOB_CONNECTION_URI}/{get_filepath(filepath, dataset_type, 'staging').split('_')[0]}"
-    country_code = filepath.split("/")[-1].split("_")[0]
-
+    country_code = filepath.split("/")[-1].split("_")[1]
+    # If a staging table already exists, how do we prevent merging files that were already merged?
     # {filepath: str, date_modified: str}
     files_for_review = []
     for file_data in adls_file_client.list_paths(
@@ -134,18 +146,22 @@ def list_staging(
 
     context.log.info(f"files_for_review: {files_for_review}")
 
+    # If silver table exists and no staging table exists, clone it to staging
+    # If silver table exists and staging table exists, merge files for review to existing staging table
+    # If silver table does not exist, merge files for review into one spark dataframe
     if DeltaTable.isDeltaTable(spark.spark_session, silver_table_path):
-        # Clone silver table to staging folder
-        silver = adls_file_client.download_delta_table_as_spark_dataframe(
-            silver_table_path, spark.spark_session
-        )
+        if not DeltaTable.isDeltaTable(spark.spark_session, staging_table_path):
+            # Clone silver table to staging folder
+            silver = adls_file_client.download_delta_table_as_spark_dataframe(
+                silver_table_path, spark.spark_session
+            )
 
-        adls_file_client.upload_spark_dataframe_as_delta_table(
-            silver,
-            staging_table_path,
-            context.op_config["dataset_type"],
-            spark.spark_session,
-        )
+            adls_file_client.upload_spark_dataframe_as_delta_table(
+                silver,
+                staging_table_path,
+                context.op_config["dataset_type"],
+                spark.spark_session,
+            )
 
         # Load new table (silver clone in staging) as a deltatable
         staging = adls_file_client.download_delta_table_as_delta_table(
@@ -158,18 +174,26 @@ def list_staging(
                 file_info["filepath"], spark.spark_session
             )
 
-            staging.alias("source").merge(
-                existing_file.alias("target"),
-                "source.school_id_giga = target.school_id_giga",
-            ).whenMatchedUpdateAll().whenNotMatchedInsertAll().execute()
+            staging = (
+                staging.alias("source")
+                .merge(
+                    existing_file.alias("target"),
+                    "source.school_id_giga = target.school_id_giga",
+                )
+                .whenMatchedUpdateAll()
+                .whenNotMatchedInsertAll()
+            )
+
+            adls_file_client.rename_file(
+                file_info["filepath"], f"{file_info['filepath']}/merged-files"
+            )
 
             context.log.info(f"Staging: table {staging}")
-        staging = (
-            staging.toDF()
-        )  # not sure if this is needed, seems like merge is in place
+
+        staging.execute()
 
     else:
-        staging = geolocation_dq_passed_rows
+        staging = list_dq_passed_rows
         # If no existing silver table, just merge the spark dataframes
         for file_date in files_for_review:
             existing_file = adls_file_client.download_delta_table_as_spark_dataframe(
@@ -177,5 +201,11 @@ def list_staging(
             )
             staging = staging.union(existing_file)
 
-    # emit_metadata_to_datahub(context, staging)
-    yield Output(staging, metadata={"filepath": get_output_filepath(context)})
+        adls_file_client.upload_spark_dataframe_as_delta_table(
+            staging,
+            staging_table_path,
+            context.op_config["dataset_type"],
+            spark.spark_session,
+        )
+
+    emit_metadata_to_datahub(context, staging)
