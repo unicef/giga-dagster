af8cada04ca66e56665a930531959e6d9a0606d6
Kenneth V. Domingo <kenneth@thinkingmachin.es>
2024-03-11 20:34:23 +0800

    feat: cache schema on migration

--- dagster/src/assets/migrations/assets.py
+++ dagster/src/assets/migrations/assets.py
@@ -4,7 +4,6 @@ from dagster_pyspark import PySparkResource
 from delta import DeltaTable
 from models import Schema
 from pyspark.sql import SparkSession
-from src.settings import settings
 from src.utils.adls import ADLSFileClient
 from src.utils.sentry import capture_op_exceptions
 
@@ -35,25 +34,26 @@ def migrate_schema(
     schema_name = Schema.__schema_name__
     filename = os.path.splitext(context.run_tags["dagster/run_key"].split("/")[-1])[0]
     table_name = filename.replace("-", "_").lower()
-    full_remote_path = f"{settings.SPARK_WAREHOUSE_DIR}/{schema_name}.db/{table_name}"
-
-    context.log.info(f"checking existence of {full_remote_path}...")
-    is_delta_table = DeltaTable.isDeltaTable(s, full_remote_path)
-    context.log.info(is_delta_table)
+    full_table_name = f"{schema_name}.{table_name}"
 
     columns = Schema.fields
     (
-        DeltaTable.createIfNotExists(s)
-        .tableName(f"{schema_name}.{table_name}")
+        DeltaTable.createOrReplace(s)
+        .tableName(full_table_name)
         .addColumns(columns)
         .execute()
     )
 
     (
-        DeltaTable.forName(s, f"{schema_name}.{table_name}")
+        DeltaTable.forName(s, full_table_name)
         .alias("master")
         .merge(df.alias("updates"), "master.name = updates.name")
         .whenMatchedUpdateAll()
         .whenNotMatchedInsertAll()
         .execute()
     )
+
+    if s.catalog.isCached(full_table_name):
+        s.catalog.refreshTable(full_table_name)
+    else:
+        s.catalog.cacheTable(full_table_name)
