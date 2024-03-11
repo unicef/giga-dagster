c593cfaea15f3ef74996e2016fe7189380901115
Renz Togonon <renz.togonon@thinkingmachin.es>
2024-03-12 00:07:47 +0800

    feat: integrate column relation checks with row level checks

--- dagster/src/data_quality_checks/utils.py
+++ dagster/src/data_quality_checks/utils.py
@@ -33,6 +33,7 @@ from src.data_quality_checks.geometry import (
 )
 from src.data_quality_checks.precision import precision_check
 from src.data_quality_checks.standard import standard_checks
+from src.data_quality_checks.column_relation import column_relation_checks
 from src.schemas import BaseSchema
 from src.spark.config_expectations import config
 from src.utils.logger import get_context_with_fallback_logger
@@ -265,14 +266,18 @@ def row_level_checks(
         df = critical_error_checks(
             df, dataset_type, CONFIG_NONEMPTY_COLUMNS[dataset_type], context
         )
+        df = column_relation_checks(df, dataset_type, context)
         df = school_density_check(df, context)
     elif dataset_type in ["coverage", "reference"]:
         df = standard_checks(df, dataset_type, context)
+        df = column_relation_checks(df, dataset_type, context)
     elif dataset_type == "coverage_fb":
         df = standard_checks(df, dataset_type, context, domain=False, range_=False)
         df = fb_percent_sum_to_100_check(df, context)
+        df = column_relation_checks(df, dataset_type, context)
     elif dataset_type == "coverage_itu":
         df = standard_checks(df, dataset_type, context, domain=False)
+        df = column_relation_checks(df, dataset_type, context)
 
     return df
 
@@ -282,118 +287,8 @@ def extract_school_id_govt_duplicates(df: sql.DataFrame):
 
     df = df.withColumn("row_num", f.row_number().over(window))
 
-    ## outputs
-    # df_duplicates = df.where(df.row_num != 1)
-    # df_duplicates = df_duplicates.drop("row_num")
-
-    # df_deduplicated = df.where(df.row_num == 1)
-    # df_deduplicated = df_deduplicated.drop("row_num")
-
     return df
 
-def column_relation_checks(df: sql.DataFrame, dataset_type: str):
-    
-    # # connectivity = 'Yes' then (connectivity_RT or connectivity_govt or download_speed_contracted) is not NULL
-    #     # expected behavior : yes -- yes|no|null/speed = 0
-    #     # expected behavior : no -- no&no&null = 0
-    #     # expected behavior : no -- no&yes&null = 1
-    # df = df.withColumn("dq_column_relation_checks-connectivity_connectivity_RT_connectivity_govt_download_speed_contracted", 
-    #     f.when(
-    #         (f.lower(f.col("connectivity")) == "yes") & (
-    #             (f.lower(f.col("connectivity_RT")) == "yes") | 
-    #             (f.lower(f.col("connectivity_govt")) == "yes") |
-    #             (f.col("download_speed_contracted").isNotNull())
-    #             ), 0,
-    #     ).when(
-    #         (f.lower(f.col("connectivity")) == "no") & (
-    #             ((f.lower(f.col("connectivity_RT")) == "no") | f.col("connectivity_RT").isNull()) & 
-    #             ((f.lower(f.col("connectivity_govt")) == "no") | f.col("connectivity_govt").isNull()) &
-    #             (f.col("download_speed_contracted").isNull())
-    #             ), 0,
-    #     ).otherwise(1)
-    # )
-    
-    # # connectivity_govt = yes download speed not null/>0?
-    # df = df.withColumn("dq_column_relation_checks-connectivity_govt_download_speed_contracted", 
-    #     f.when(
-    #         (f.col("download_speed_contracted").isNotNull()) &
-    #         (f.col("connectivity_govt").isNull())
-    #         , 1,
-    #     ).otherwise(0)
-    # )
-    
-    # # Connectivity_RT -- connectivity_RT_datasource -- connectivity_RT_ingestion_timestamp if 1 present all are present
-    # df = df.withColumn("dq_column_relation_checks-connectivity_RT_connectivity_RT_datasource_connectivity_RT_ingestion_timestamp", 
-    #         f.when(
-    #             (f.col("connectivity_RT").isNull()) &
-    #             (f.col("connectivity_RT_datasource").isNull()) &
-    #             (f.col("connectivity_RT_ingestion_timestamp").isNull())
-    #             , 0,
-    #         ).when(
-    #             (f.col("connectivity_RT").isNotNull()) &
-    #             (f.col("connectivity_RT_datasource").isNotNull()) &
-    #             (f.col("connectivity_RT_ingestion_timestamp").isNotNull())
-    #             , 0,
-    #         ).otherwise(1)
-    #     )
-    
-    # # cellular_coverage_availability -- cellular_coverage_type
-    # df = df.withColumn("dq_column_relation_checks-cellular_coverage_availability_cellular_coverage_type",
-    #         f.when(
-    #             (f.lower(f.col("cellular_coverage_availability")) == "yes") &
-    #             (f.lower(f.col("cellular_coverage_type")).isin(["2g", "3g", "4g", "5g"]))
-    #             , 0,
-    #         ).when(
-    #             (f.lower(f.col("cellular_coverage_availability")) == "no") &
-    #             (f.lower(f.col("cellular_coverage_type")) == "no coverage")
-    #             , 0,
-    #         ).when(
-    #             (f.col("cellular_coverage_availability").isNull()) &
-    #             (f.col("cellular_coverage_type").isNull())   
-    #             , 0,
-    #         ).otherwise(1)
-    # )
-
-    # # connectivity_govt -- connectivity_govt_ingestion_timestamp if connectivity_govt yes, timestamp isnotnull
-    # df = df.withColumn("dq_column_relation_checks-connectivity_govt_connectivity_govt_ingestion_timestamp",
-    #         f.when(
-    #             (f.lower(f.col("connectivity_govt")) == "yes") &
-    #             (f.col("connectivity_govt_ingestion_timestamp").isNull())
-    #             , 1,
-    #         ).otherwise(0)
-    # )
-
-    # electricity_availability -- electricity_type if Electricity_availability yes, electricity_type isnotnull
-    df = df.withColumn("dq_column_relation_checks-connectivity_govt_connectivity_govt_ingestion_timestamp",
-            f.when(
-                (f.lower(f.col("electricity_availability")) == "yes") &
-                (f.col("electricity_type").isNull())
-                , 1,
-            ).otherwise(0)
-    )
-
-    # # nearest_XX_distance -- nearest_XX_id
-    # column_pairs = {
-    #     ("nearest_NR_id", "nearest_NR_distance"),
-    #     ("nearest_LTE_id", "nearest_LTE_distance"),
-    #     ("nearest_UMTS_id", "nearest_UMTS_distance"),
-    #     ("nearest_GSM_id", "nearest_GSM_distance"),
-    # }
-
-    # for id, distance in column_pairs:
-    #     df = df.withColumn(f"dq_column_relation_checks-{id}_{distance}", 
-    #             f.when(
-    #                 (f.col(f"{id}").isNull()) &
-    #                 (f.col(f"{distance}").isNull())
-    #                 , 0,
-    #             ).when(
-    #                 (f.col(f"{id}").isNotNull()) &
-    #                 (f.col(f"{distance}").isNotNull())
-    #                 , 0,
-    #             ).otherwise(1)
-    #         )  
-
-    return df
 
 if __name__ == "__main__":
     from src.utils.spark import get_spark_session
@@ -418,15 +313,15 @@ if __name__ == "__main__":
     # df_bronze = df_bronze.select(*["connectivity", "connectivity_RT", "connectivity_govt", "download_speed_contracted", "connectivity_RT_datasource","connectivity_RT_ingestion_timestamp"])
     # df_bronze = df_bronze.select(*["connectivity_govt", "connectivity_govt_ingestion_timestamp"])
     # df_bronze = df_bronze.select(*["nearest_NR_id", "nearest_NR_distance","nearest_LTE_id", "nearest_LTE_distance","nearest_UMTS_id", "nearest_UMTS_distance","nearest_GSM_id", "nearest_GSM_distance"])
-    df_bronze = df_bronze.select(*["electricity_availability", "electricity_type"])
-    df_bronze.show()
+    # df_bronze = df_bronze.select(*["electricity_availability", "electricity_type"])
+    # df_bronze.show()
     # df = standard_checks(df_bronze, 'master')
     # df_bronze = df_bronze.withColumn("school_id_giga", f.lit("9663bb61-6ad9-3d91-9a16-90e8c40448142"))
     # df = format_validation_checks(df_bronze)
-    df = column_relation_checks(df_bronze, 'master')
+    df = row_level_checks(df_bronze, 'coverage', 'GIN')
     # df_bronze = df_bronze.withColumn("school_id_govt", f.lit("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Donec elementum dignissim magna, eu efficitur libero congue sit amet. Morbi posuere, quam ac convallis laoreet, ipsum elit condimentum arcu, nec sollicitudin lorem odio id nunc. Nulla facilisi. Quisque ut efficitur nisi. Vestibulum bibendum posuere elit ac vestibulum. Nullam ultrices magna nec arcu ullamcorper, a luctus eros volutpat. Proin vel libero vitae velit feugiat malesuada nec ut felis. In hac habitasse platea dictumst. Fusce euismod vestibulum lorem, ac venenatis sapien efficitur non. Sed tempor nunc sit amet velit malesuada, quis bibendum odio dictum."))
     # df = standard_checks(df_bronze, 'master')
-    df.distinct().show()
+    # df.distinct().show()
     
     # column_pairs = {
     #     ("nearest_NR_id", "nearest_NR_distance"),
@@ -441,8 +336,8 @@ if __name__ == "__main__":
     # # df = dq_passed_rows(df, "coverage")
     # df.orderBy("column").show()
 
-    # df = aggregate_report_spark_df(spark=spark, df=df)
-    # df.show()
+    df = aggregate_report_spark_df(spark=spark, df=df)
+    df.show()
 
-    # _json = aggregate_report_json(df, df_bronze)
-    # print(_json)
+    _json = aggregate_report_json(df, df_bronze)
+    print(_json)
