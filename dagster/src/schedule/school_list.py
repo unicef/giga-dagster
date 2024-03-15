88cab15db7eba401496aee5fe13f326ed31cf90d
tm-erin-cheng <erin@thinkingmachin.es>
2024-03-15 21:11:16 +0900

    fix: schedules

--- dagster/src/schedule/school_list.py
+++ dagster/src/schedule/school_list.py
@@ -6,7 +6,6 @@ from src.jobs.qos import qos_school_list__automated_data_checks_job
 from src.utils.db import get_db_context
 
 
-# one schedule for each freq of ingestion
 @schedule(job=qos_school_list__automated_data_checks_job, cron_schedule="*/15 * * * *")
 def qos_school_list__schedule(context: ScheduleEvaluationContext):
     scheduled_date = context.scheduled_execution_time.strftime("%Y-%m-%d")
@@ -14,19 +13,21 @@ def qos_school_list__schedule(context: ScheduleEvaluationContext):
     with get_db_context() as session:
         school_list_apis = session.scalars(select(SchoolList).where(SchoolList.enabled))
 
-        for api in school_list_apis:
-            config = api.__dict__
+    ops_list = [
+        "qos_school_list_raw",
+        "qos_school_list_bronze",
+        "qos_school_list_data_quality_results",
+        "qos_school_list_dq_passed_rows",
+        "qos_school_list_dq_failed_rows",
+        "qos_school_list_staging",
+    ]
 
-            yield RunRequest(
-                run_key=f"{config['name']}_{scheduled_date}",
-                run_config=RunConfig(
-                    ops={
-                        "qos_school_list_raw": config,
-                        "qos_school_list_bronze": config,
-                        "qos_school_list_data_quality_results": config,
-                        "qos_school_list_dq_passed_rows": config,
-                        "qos_school_list_dq_failed_rows": config,
-                        "qos_school_list_staging": config,
-                    }
-                ),
-            )
+    for api in school_list_apis:
+        config = api.__dict__
+
+        yield RunRequest(
+            run_key=f"{config['name']}_{scheduled_date}",
+            run_config=RunConfig(
+                ops=dict(zip(ops_list, [config] * len(ops_list), strict=True))
+            ),
+        )
