8b2ae9c83d247cf833b5edc38be2b00837dbcc32
Kenneth V. Domingo <kenneth@thinkingmachin.es>
2024-03-19 19:05:07 +0800

    refactor: use AssetSelection notation in jobs

--- dagster/src/jobs/school_master.py
+++ dagster/src/jobs/school_master.py
@@ -3,44 +3,21 @@ from src.hooks.school_master import geolocation_dq_checks_location_db_update_hoo
 
 school_master_geolocation__automated_data_checks_job = define_asset_job(
     name="school_master_geolocation__automated_data_checks_job",
-    selection=[
-        # TODO: Replace with AssetSelection notation
-        "geolocation_raw",
-        "geolocation_bronze",
-        "geolocation_data_quality_results",
-        "geolocation_data_quality_results_summary",
-        "geolocation_dq_passed_rows",
-        "geolocation_dq_failed_rows",
-        # "geolocation_staging",
-    ],
+    # TODO: Include staging
+    selection="*geolocation_dq_failed_rows",
     hooks={geolocation_dq_checks_location_db_update_hook},
 )
 
 school_master_coverage__automated_data_checks_job = define_asset_job(
     name="school_master_coverage__automated_data_checks_job",
-    selection=[
-        # TODO: Replace with AssetSelection notation
-        "coverage_raw",
-        "coverage_dq_results",
-        "coverage_dq_summary_statistics",
-        # "coverage_dq_checks",
-        "coverage_dq_passed_rows",
-        "coverage_dq_failed_rows",
-        "coverage_bronze",
-        # "coverage_staging",
-    ],
+    # TODO: Include staging
+    selection="*coverage_bronze",
 )
 
 
 school_master_geolocation__successful_manual_checks_job = define_asset_job(
     name="school_master_geolocation__successful_manual_checks_job",
-    selection=[
-        # TODO: Replace with AssetSelection notation
-        "manual_review_passed_rows",
-        "silver",
-        "master",
-        "reference",
-    ],
+    selection="manual_review_passed_rows*",
 )
 
 
@@ -52,13 +29,7 @@ school_master_geolocation__failed_manual_checks_job = define_asset_job(
 
 school_master_coverage__successful_manual_checks_job = define_asset_job(
     name="school_master_coverage__successful_manual_checks_job",
-    selection=[
-        # TODO: Replace with AssetSelection notation
-        "manual_review_passed_rows",
-        "silver",
-        "master",
-        "reference",
-    ],
+    selection="manual_review_passed_rows*",
 )
 
 
