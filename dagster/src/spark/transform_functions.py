9d4c4c052d49cdf93961330f789bc452be447c36
Renz Togonon <renz.togonon@thinkingmachin.es>
2024-03-27 16:41:37 +0800

    feat: mapbox transforms testing

--- dagster/src/spark/transform_functions.py
+++ dagster/src/spark/transform_functions.py
@@ -1,12 +1,21 @@
+import io
 import uuid
 
+import geopandas as gpd
 import h3
 from pyspark import sql
 from pyspark.sql import functions as f
 from pyspark.sql.types import ArrayType, NullType, StringType, StructField
 
+from azure.storage.blob import BlobServiceClient
 from src.settings import settings
 from src.spark.config_expectations import config
+from src.spark.udf_dependencies import get_point
+
+ACCOUNT_URL = "https://saunigiga.blob.core.windows.net/"
+azure_sas_token = settings.AZURE_SAS_TOKEN
+azure_blob_container_name = settings.AZURE_BLOB_CONTAINER_NAME
+container_name = azure_blob_container_name
 
 
 # STANDARDIZATION FUNCTIONS
@@ -243,50 +252,103 @@ def point_110(column):
 point_110_udf = f.udf(point_110)
 
 
+def get_admin1_boundaries(country_code_iso3: str):
+    try:
+        service = BlobServiceClient(account_url=ACCOUNT_URL, credential=azure_sas_token)
+        filename = f"{country_code_iso3}_admin1.geojson"
+        file = f"mapbox_sample_data/{filename}"
+        blob_client = service.get_blob_client(container=container_name, blob=file)
+        with io.BytesIO() as file_blob:
+            download_stream = blob_client.download_blob()
+            download_stream.readinto(file_blob)
+            file_blob.seek(0)
+            admin1_boundaries = gpd.read_file(file_blob)
+
+    except:  # noqa: E722
+        return None
+
+    return admin1_boundaries
+
+
+def get_admin1_columns(latitude: float, longitude: float, admin1_boundaries):
+    point = get_point(longitude=longitude, latitude=latitude)
+
+    if point is not None and admin1_boundaries is not None:
+        for index, row in admin1_boundaries.iterrows():  # noqa: B007
+            if row.geometry.contains(point):
+                admin1_en = row["name_en"]
+                admin1_native = row["name"]
+                admin1_id_giga = row["admin1_id_giga"]
+                break
+            else:
+                admin1_en = None
+                admin1_native = None
+                admin1_id_giga = None
+    else:
+        admin1_en = None
+        admin1_native = None
+        admin1_id_giga = None
+
+    return admin1_en, admin1_native, admin1_id_giga
+
+
+def create_admin1_columns(latitude: float, longitude: float, country_code_iso3: str):
+    boundaries = get_admin1_boundaries(country_code_iso3)
+    admin1_en, admin1_native, admin1_id_giga = get_admin1_columns(
+        latitude, longitude, boundaries
+    )
+    return print(admin1_en), print(admin1_native), print(admin1_id_giga)
+
+
 if __name__ == "__main__":
     from src.utils.spark import get_spark_session
 
     #
     # file_url = f"{settings.AZURE_BLOB_CONNECTION_URI}/bronze/school-geolocation-data/BLZ_school-geolocation_gov_20230207.csv"
-    file_url_master = f"{settings.AZURE_BLOB_CONNECTION_URI}/updated_master_schema/master/BLZ_school_geolocation_coverage_master.csv"
-    file_url_reference = f"{settings.AZURE_BLOB_CONNECTION_URI}/updated_master_schema/reference/BLZ_master_reference.csv"
+    file_url_master = f"{settings.AZURE_BLOB_CONNECTION_URI}/updated_master_schema/master/BRA_school_geolocation_coverage_master.csv"
+    # file_url_reference = f"{settings.AZURE_BLOB_CONNECTION_URI}/updated_master_schema/reference/BLZ_master_reference.csv"
     # file_url = f"{settings.AZURE_BLOB_CONNECTION_URI}/adls-testing-raw/_test_BLZ_RAW.csv"
 
     spark = get_spark_session()
     master = spark.read.csv(file_url_master, header=True)
-    reference = spark.read.csv(file_url_reference, header=True)
-    df_bronze = master.join(reference, how="left", on="school_id_giga")
+    # reference = spark.read.csv(file_url_reference, header=True)
+    # df_bronze = master.join(reference, how="left", on="school_id_giga")
 
     # df = spark.read.csv(file_url, header=True)
     # df = create_bronze_layer_columns(df)
     # df.show()
 
-    # Create DataFrame
-    # data = [(i, 1) for i in range(10)]
-    # df = spark.createDataFrame(data, ["id", "code"])
+    # df = master.select(
+    #     [
+    #         "school_id_giga",
+    #         "school_id_govt",
+    #         "school_name",
+    #         "education_level",
+    #         "latitude",
+    #         "longitude",
+    #         "admin1",
+    #         "admin2"
+    #     ]
+    # ).limit(20)
+    # df = df.withColumn("school_name", f.trim(f.col("school_name")))
+    # df = df.withColumn("latitude", f.lit(17.5066649))
+    # df = create_school_id_giga(df)
+    # df = df.filter(f.col("school_id_giga") == f.col("school_id_giga_test"))
+    # df = is_not_within_country(df=df, country_code_iso3="GIN")
+
+    # df = df.withColumn("country_code_iso3", f.lit("BRA"))
+    # df = df.withColumn("admin1_test", get_admin1(f.col("latitude"), f.col("longitude"), f.col("country_code_iso3")))
     # df.show()
-    columnMapping = {
-        "school_id_govt": "id",
-        "school_name": "code",
-    }
-    # df = column_mapping_rename(df, columnMapping)
-    # df = impute_geolocation_columns(df)
-    df_bronze = df_bronze.sort("school_name").limit(30)
-    df = df_bronze.select(
-        [
-            "school_id_giga",
-            "school_id_govt",
-            "school_name",
-            "education_level",
-            "latitude",
-            "longitude",
-        ]
-    )
-    df = df.withColumn("school_name", f.trim(f.col("school_name")))
-    df = df.filter(f.col("school_id_govt") == "P12001")
-    df = df.withColumn("latitude", f.lit(17.5066649))
-    df = create_school_id_giga(df)
-    df.show()
+
+    # admin1_boundaries = get_admin1_boundaries(country_code_iso3="BRA")
+    # latitude = -8.758459
+    # longitude = -63.85401
+    # print(get_admin1_columns(latitude=latitude, longitude=longitude, admin1_boundaries=admin1_boundaries))
+
+    # print(get_admin1_columns(-8.758459, -63.85401, get_admin1_boundaries("BRA")))
+    create_admin1_columns(-8.758459, -63.85401, "BRA")
+    create_admin1_columns(8.758459, -63.85401, "BRA")
+    create_admin1_columns(8.758459, -63.85401, "BRI")
 
     # print(len(df.columns))
     # list_inventory = [
