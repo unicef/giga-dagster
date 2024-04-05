3047d283f7292c8c859350471512e9de7855255c
tm-sofia-pineda <sofia.pineda@thinkingmachin.es>
2024-04-05 15:58:43 +0800

    fix: bug when assertion column is empty

--- dagster/src/utils/datahub/create_validation_tab.py
+++ dagster/src/utils/datahub/create_validation_tab.py
@@ -75,7 +75,7 @@ class EmitDatasetAssertionResults:
 
     @staticmethod
     def extract_assertion_info_of_column(column_dq_result: dict, dataset_urn: str):
-        if "column" in column_dq_result.keys():
+        if column_dq_result["column"] != "":
             field_urn = builder.make_schema_field_urn(
                 dataset_urn, field_path=column_dq_result["column"]
             )
