5e3891bb4e2dc6b0a399529417b54225884ee869
tm-sofia-pineda <sofia.pineda@thinkingmachin.es>
2024-04-12 21:33:24 +0800

    feat-fix: access policyfilter  by dataset tag and dataset type

--- dagster/src/utils/datahub/update_policies.py
+++ dagster/src/utils/datahub/update_policies.py
@@ -4,20 +4,26 @@ from urllib import parse
 import country_converter as cc
 from datahub.ingestion.graph.client import DatahubClientConfig, DataHubGraph
 from src.settings import settings
+from src.utils.logger import get_context_with_fallback_logger
 
 
-def policy_mutation_query(country_name, group_urn):
-    datasets_urns_list = list_datasets_by_tag(tag=country_name)
+def policy_mutation_query(group_urn):
+    group_name = parse.unquote(group_urn.split("urn:li:corpGroup:")[1])
+    country_name = group_name.split("-")[0]
+    dataset_type = group_name.split(" ")[1].lower()
+    datasets_urns_list = list_datasets_by_filter(
+        tag=country_name, dataset_type=dataset_type
+    )
 
     query = f"""
     mutation {{
         updatePolicy(
-            urn: "urn:li:dataHubPolicy:{country_name}-viewer",
+            urn: "urn:li:dataHubPolicy:{group_name}-viewer",
             input: {{
                 type: METADATA,
-                name: "{country_name} - VIEWER",
+                name: "{group_name} - VIEWER",
                 state: ACTIVE,
-                description: "Members can view datasets with country name: {country_name}.",
+                description: "Members can view datasets with country name: {group_name}.",
                 resources: {{
                     resources: {datasets_urns_list},
                     allResources: true,
@@ -36,35 +42,66 @@ def policy_mutation_query(country_name, group_urn):
     return query
 
 
-def list_datasets_by_tag(tag):
+def create_policy_query(group_urn):
+    group_name = parse.unquote(group_urn.split("urn:li:corpGroup:")[1])
+    country_name = group_urn.split("urn:li:corpGroup:")[1].split("-")[0]
+    dataset_type = group_name.split(" ")[1].lower()
+    datasets_urns_list = list_datasets_by_filter(
+        tag=country_name, dataset_type=dataset_type
+    )
+
+    query = f"""
+        mutation {{
+            createPolicy(
+                input: {{
+                    type: METADATA,
+                    name: "{group_name} - VIEWER",
+                    state: ACTIVE,
+                    description: "Members can view datasets in {group_name}.",
+                    resources: {{
+                        resources: {datasets_urns_list},
+                        allResources: true,
+                    }},
+                    privileges: ["VIEW_ENTITY_PAGE", "VIEW_DATASET_USAGE", "VIEW_DATASET_PROFILE"],
+                    actors: {{
+                        groups: ["{group_urn}"],
+                        resourceOwners: true,
+                        allUsers: false,
+                        allGroups: false
+                    }}
+            }})
+        }}
+        """
+    return query
+
+
+def list_datasets_by_filter(tag: str, dataset_type: str):
     datahub_graph_client = DataHubGraph(
         DatahubClientConfig(
             server=settings.DATAHUB_METADATA_SERVER_URL,
             token=settings.DATAHUB_ACCESS_TOKEN,
         )
     )
-
-    search_query = f"""query {{
-        search (input:{{
-            type: DATASET,
-            query: "tag:{tag}",
-        }}) {{
-            searchResults {{
-                entity {{
-                    urn
-                }}
-            }}
-        }}
-    }}"""
-
-    search_results = datahub_graph_client.execute_graphql(query=search_query)
-    results = search_results["search"]["searchResults"]
-
-    urn_list = []
-    for result in results:
-        urn_list.append(f"{result['entity']['urn']}")
-
+    query = f"tag:{tag}"
+    dataset_urns_iterator = datahub_graph_client.get_urns_by_filter(
+        entity_types=["dataset"],
+        query=query,
+        extraFilters=[
+            {"field": "urn", "values": [dataset_type], "condition": "CONTAIN"}
+        ],
+    )
+    urn_list = list(dataset_urns_iterator)
     return json.dumps(urn_list)
+
+
+def group_urns_iterator():
+    datahub_graph_client = DataHubGraph(
+        DatahubClientConfig(
+            server=settings.DATAHUB_METADATA_SERVER_URL,
+            token=settings.DATAHUB_ACCESS_TOKEN,
+        )
+    )
+    return datahub_graph_client.get_urns_by_filter(entity_types=["corpGroup"])
 
 
 def is_valid_country_name(country_name):
@@ -73,7 +110,7 @@ def is_valid_country_name(country_name):
     return country_name in country_list
 
 
-def update_policies():
+def update_policies(context=None):
     datahub_graph_client = DataHubGraph(
         DatahubClientConfig(
             server=settings.DATAHUB_METADATA_SERVER_URL,
@@ -81,25 +118,58 @@ def update_policies():
         )
     )
 
-    list_allgroups_query = """
-        query {
-            listGroups(input: {}) {
-                groups {
-                    urn
-                    name
-                }
-            }
-        }
-    """
-
-    groups_list = datahub_graph_client.execute_graphql(query=list_allgroups_query)
-
-    for group in groups_list["listGroups"]["groups"]:
-        group_urn = group["urn"]
-        country_name = parse.unquote(group["name"])
-
+    for group_urn in group_urns_iterator():
+        country_name = group_urn.split("urn:li:corpGroup:")[1].split("-")[0]
         if is_valid_country_name(country_name):
-            query = policy_mutation_query(
-                country_name=country_name, group_urn=group_urn
-            )
+            try:
+                query = policy_mutation_query(group_urn=group_urn)
+                get_context_with_fallback_logger(context).info(
+                    "UPDATING DATAHUB POLICY..."
+                )
+                get_context_with_fallback_logger(context).info(query)
+                datahub_graph_client.execute_graphql(query=query)
+            except Exception as error:
+                get_context_with_fallback_logger(context).error(error)
+                try:
+                    query = create_policy_query(group_urn=group_urn)
+                    get_context_with_fallback_logger(context).info(
+                        "CREATING DATAHUB POLICY..."
+                    )
+                    get_context_with_fallback_logger(context).info(query)
+                    datahub_graph_client.execute_graphql(query=query)
+                except Exception:
+                    get_context_with_fallback_logger(context).error(error)
+
+
+def update_specific_policy(group_urn: str, context=None):
+    datahub_graph_client = DataHubGraph(
+        DatahubClientConfig(
+            server=settings.DATAHUB_METADATA_SERVER_URL,
+            token=settings.DATAHUB_ACCESS_TOKEN,
+        )
+    )
+
+    country_name = group_urn.split("urn:li:corpGroup:")[1].split("-")[0]
+    if is_valid_country_name(country_name):
+        try:
+            query = policy_mutation_query(group_urn=group_urn)
+            get_context_with_fallback_logger(context).info("UPDATING DATAHUB POLICY...")
+            get_context_with_fallback_logger(context).info(query)
             datahub_graph_client.execute_graphql(query=query)
+        except Exception as error:
+            get_context_with_fallback_logger(context).error(error)
+            try:
+                query = create_policy_query(group_urn=group_urn)
+                get_context_with_fallback_logger(context).info(
+                    "CREATING DATAHUB POLICY..."
+                )
+                get_context_with_fallback_logger(context).info(query)
+                datahub_graph_client.execute_graphql(query=query)
+            except Exception:
+                get_context_with_fallback_logger(context).error(error)
+
+
+if __name__ == "__main__":
+    print(list_datasets_by_filter(tag="Benin", dataset_type="geolocation"))
+    # print(list_all_groups())
+    pass
