7c90d9344dfd6fbede054ecd9a2609a037537e7e
Kenneth V. Domingo <kenneth@thinkingmachin.es>
2024-04-18 13:41:31 +0800

    fix: remove default schedule running status for prod

--- dagster/src/schedule/datahub.py
+++ dagster/src/schedule/datahub.py
@@ -1,4 +1,4 @@
-from dagster import DefaultScheduleStatus, ScheduleDefinition
+from dagster import ScheduleDefinition
 from src.jobs.datahub import (
     datahub__ingest_coverage_notebooks_from_github_job,
     datahub__materialize_prerequisites_job,
@@ -9,23 +9,14 @@ from src.settings import settings
 datahub_materialize_prerequisities_schedule = ScheduleDefinition(
     job=datahub__materialize_prerequisites_job,
     cron_schedule="0 0 1 1 *",  # yearly
-    default_status=DefaultScheduleStatus.RUNNING
-    if settings.IN_PRODUCTION
-    else DefaultScheduleStatus.STOPPED,
 )
 
 datahub_update_access_schedule = ScheduleDefinition(
     job=datahub__update_access_job,
     cron_schedule=settings.DEFAULT_SCHEDULE_CRON,
-    default_status=DefaultScheduleStatus.RUNNING
-    if settings.IN_PRODUCTION
-    else DefaultScheduleStatus.STOPPED,
 )
 
 datahub_ingest_coverage_notebooks_schedule = ScheduleDefinition(
     job=datahub__ingest_coverage_notebooks_from_github_job,
     cron_schedule="0 0 1 * *",  # monthly
-    default_status=DefaultScheduleStatus.RUNNING
-    if settings.IN_PRODUCTION
-    else DefaultScheduleStatus.STOPPED,
 )
