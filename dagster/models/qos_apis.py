a29169c1f70db53cf39547ccdf3b5403e8493fec
tm-erin-cheng <erin@thinkingmachin.es>
2024-04-23 16:59:28 +0800

    chore: change models to align w data ingestion

--- dagster/models/qos_apis.py
+++ dagster/models/qos_apis.py
@@ -1,7 +1,10 @@
 import enum
+from datetime import datetime
 
-from sqlalchemy import JSON, ForeignKey, String
+from pydantic import EmailStr
+from sqlalchemy import JSON, DateTime, Enum, ForeignKey, String
 from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy.sql import func
 
 from models.base_database import BaseModel
 
@@ -30,41 +33,79 @@ class SendQueryInEnum(enum.Enum):
     NONE = "NONE"
 
 
+class SendDateInEnum(enum.Enum):
+    BODY = "BODY"
+    QUERY_PARAMETERS = "QUERY_PARAMETERS"
+
+
+class ApiConfiguration(BaseModel):
+    __abstract__ = True
+
+    api_auth_api_key: Mapped[str] = mapped_column(nullable=True)
+    api_auth_api_value: Mapped[str] = mapped_column(nullable=True)
+    api_endpoint: Mapped[str] = mapped_column(nullable=False)
+    authorization_type: Mapped[AuthorizationTypeEnum] = mapped_column(
+        Enum(AuthorizationTypeEnum), default=AuthorizationTypeEnum.NONE, nullable=False
+    )
+    basic_auth_password: Mapped[str] = mapped_column(nullable=True)
+    basic_auth_username: Mapped[str] = mapped_column(nullable=True)
+    bearer_auth_bearer_token: Mapped[str] = mapped_column(nullable=True)
+    data_key: Mapped[str] = mapped_column(nullable=True)
+    date_created: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), server_default=func.now()
+    )
+    date_modified: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), server_default=func.now(), onupdate=func.now()
+    )
+    date_last_ingested: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), server_default=func.now(), onupdate=func.now()
+    )
+
+    date_last_successfully_ingested: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), server_default=func.now(), onupdate=func.now()
+    )
+    enabled: Mapped[bool] = mapped_column(default=True)
+    error_message: Mapped[str] = mapped_column(nullable=True)
+    page_number_key: Mapped[str] = mapped_column(nullable=True)
+    page_offset_key: Mapped[str] = mapped_column(nullable=True)
+    page_send_query_in: Mapped[SendQueryInEnum] = mapped_column(
+        Enum(SendQueryInEnum), default=SendQueryInEnum.NONE, nullable=False
+    )
+    page_size_key: Mapped[str] = mapped_column(nullable=True)
+    page_starts_with: Mapped[int] = mapped_column(nullable=True)
+    pagination_type: Mapped[PaginationTypeEnum] = mapped_column(
+        Enum(PaginationTypeEnum), default=PaginationTypeEnum.NONE, nullable=False
+    )
+    query_parameters: Mapped[dict] = mapped_column(
+        JSON, nullable=True, server_default="{}"
+    )
+    request_body: Mapped[dict] = mapped_column(JSON, nullable=True, server_default="{}")
+    request_method: Mapped[RequestMethodEnum] = mapped_column(
+        Enum(RequestMethodEnum), default=RequestMethodEnum.GET, nullable=False
+    )
+    school_id_key: Mapped[str] = mapped_column(nullable=False)
+    school_id_send_query_in: Mapped[SendQueryInEnum] = mapped_column(
+        Enum(SendQueryInEnum), default=SendQueryInEnum.NONE, nullable=False
+    )
+    size: Mapped[int] = mapped_column(nullable=True)
+
+
 # class ApiConfiguration(BaseModel):
 #     __abstract__ = True
 
 #     api_auth_api_key: Mapped[str] = mapped_column(nullable=True)
 #     api_auth_api_value: Mapped[str] = mapped_column(nullable=True)
 #     api_endpoint: Mapped[str] = mapped_column(nullable=True)
-#     authorization_type: Mapped[AuthorizationTypeEnum] = mapped_column(
-#         Enum(AuthorizationTypeEnum),
-#         default=AuthorizationTypeEnum.NONE,
-#         nullable=True,
-#     )
+#     authorization_type: Mapped[str] = mapped_column(nullable=True)
 #     basic_auth_password: Mapped[str] = mapped_column(nullable=True)
 #     basic_auth_username: Mapped[str] = mapped_column(nullable=True)
 #     bearer_auth_bearer_token: Mapped[str] = mapped_column(nullable=True)
 
 #     data_key: Mapped[str] = mapped_column(nullable=True)
-#     date_created: Mapped[str] = mapped_column(
-#         DateTime(timezone=True),
-#         server_default=func.now(),
-#     )
-#     date_modified: Mapped[str] = mapped_column(
-#         DateTime(timezone=True),
-#         server_default=func.now(),
-#         onupdate=func.now(),
-#     )
-#     date_last_successfully_ingested: Mapped[str] = mapped_column(
-#         DateTime(timezone=True),
-#         server_default=func.now(),
-#         onupdate=func.now(),
-#     )
-#     date_last_ingested: Mapped[str] = mapped_column(
-#         DateTime(timezone=True),
-#         server_default=func.now(),
-#         onupdate=func.now(),
-#     )
+#     date_created: Mapped[str] = mapped_column(nullable=True)
+#     date_modified: Mapped[str] = mapped_column(nullable=True)
+#     date_last_successfully_ingested: Mapped[str] = mapped_column(nullable=True)
+#     date_last_ingested: Mapped[str] = mapped_column(nullable=True)
 
 #     enabled: Mapped[bool] = mapped_column(default=True)
 #     error_message: Mapped[str] = mapped_column(nullable=True)
@@ -73,89 +114,30 @@ class SendQueryInEnum(enum.Enum):
 #     page_offset_key: Mapped[str] = mapped_column(nullable=True)
 #     page_size_key: Mapped[str] = mapped_column(nullable=True)
 #     page_starts_with: Mapped[int] = mapped_column(nullable=True)
-#     pagination_type: Mapped[PaginationTypeEnum] = mapped_column(
-#         Enum(PaginationTypeEnum),
-#         default=PaginationTypeEnum.NONE,
-#         nullable=True,
-#     )
+#     pagination_type: Mapped[str] = mapped_column(nullable=True)
 
 #     query_parameters: Mapped[dict] = mapped_column(JSON, nullable=True)
 #     request_body: Mapped[dict] = mapped_column(JSON, nullable=True)
 
-#     request_method: Mapped[RequestMethodEnum] = mapped_column(
-#         Enum(RequestMethodEnum),
-#         default=RequestMethodEnum.GET,
-#         nullable=True,
-#     )
+#     request_method: Mapped[str] = mapped_column(nullable=True)
 #     school_id_key: Mapped[str] = mapped_column(nullable=True)
-#     page_send_query_in: Mapped[SendQueryInEnum] = (
-#         mapped_column(
-#             Enum(SendQueryInEnum),
-#             default=SendQueryInEnum.NONE,
-#             nullable=True,
-#         ),
-#     )
-#     school_id_send_query_in: Mapped[SendQueryInEnum] = (
-#         mapped_column(
-#             Enum(SendQueryInEnum),
-#             default=SendQueryInEnum.NONE,
-#             nullable=True,
-#         ),
-#     )
+#     page_send_query_in: Mapped[str] = mapped_column(nullable=True)
+#     school_id_send_query_in: Mapped[str] = mapped_column(nullable=True)
 #     size: Mapped[int] = mapped_column(nullable=True)
-
-
-class ApiConfiguration(BaseModel):
-    __abstract__ = True
-
-    api_auth_api_key: Mapped[str] = mapped_column(nullable=True)
-    api_auth_api_value: Mapped[str] = mapped_column(nullable=True)
-    api_endpoint: Mapped[str] = mapped_column(nullable=True)
-    authorization_type: Mapped[str] = mapped_column(nullable=True)
-    basic_auth_password: Mapped[str] = mapped_column(nullable=True)
-    basic_auth_username: Mapped[str] = mapped_column(nullable=True)
-    bearer_auth_bearer_token: Mapped[str] = mapped_column(nullable=True)
-
-    data_key: Mapped[str] = mapped_column(nullable=True)
-    date_created: Mapped[str] = mapped_column(nullable=True)
-    date_modified: Mapped[str] = mapped_column(nullable=True)
-    date_last_successfully_ingested: Mapped[str] = mapped_column(nullable=True)
-    date_last_ingested: Mapped[str] = mapped_column(nullable=True)
-
-    enabled: Mapped[bool] = mapped_column(default=True)
-    error_message: Mapped[str] = mapped_column(nullable=True)
-
-    page_number_key: Mapped[str] = mapped_column(nullable=True)
-    page_offset_key: Mapped[str] = mapped_column(nullable=True)
-    page_size_key: Mapped[str] = mapped_column(nullable=True)
-    page_starts_with: Mapped[int] = mapped_column(nullable=True)
-    pagination_type: Mapped[str] = mapped_column(nullable=True)
-
-    query_parameters: Mapped[dict] = mapped_column(JSON, nullable=True)
-    request_body: Mapped[dict] = mapped_column(JSON, nullable=True)
-
-    request_method: Mapped[str] = mapped_column(nullable=True)
-    school_id_key: Mapped[str] = mapped_column(nullable=True)
-    page_send_query_in: Mapped[str] = mapped_column(nullable=True)
-    school_id_send_query_in: Mapped[str] = mapped_column(nullable=True)
-    size: Mapped[int] = mapped_column(nullable=True)
 
 
 class SchoolList(ApiConfiguration):
     __tablename__ = "qos_school_list"
 
     column_to_schema_mapping: Mapped[dict] = mapped_column(
-        JSON,
-        nullable=True,
-        server_default="",
+        JSON, nullable=False, server_default="{}"
     )
-    name: Mapped[str] = mapped_column(nullable=True, server_default="")
-    user_email: Mapped[str] = mapped_column(String(), nullable=True)
-    user_id: Mapped[str] = mapped_column(nullable=True)
+    name: Mapped[str] = mapped_column(nullable=False, server_default="")
+    user_email: Mapped[EmailStr] = mapped_column(String(), nullable=False)
+    user_id: Mapped[str] = mapped_column(nullable=False)
 
     school_connectivity: Mapped["SchoolConnectivity"] = relationship(
-        "SchoolConnectivity",
-        back_populates="school_list",
+        "SchoolConnectivity", back_populates="school_list"
     )
 
 
@@ -163,7 +145,7 @@ class SchoolConnectivity(ApiConfiguration):
     __tablename__ = "qos_school_connectivity"
 
     ingestion_frequency_minutes: Mapped[int] = mapped_column()
-    schema_url: Mapped[str] = mapped_column()
+    schema_url: Mapped[str] = mapped_column(nullable=True)
 
     school_list_id: Mapped[str] = mapped_column(ForeignKey("qos_school_list.id"))
     school_list: Mapped["SchoolList"] = relationship(
@@ -172,6 +154,8 @@ class SchoolConnectivity(ApiConfiguration):
 
     date_key: Mapped[str] = mapped_column(nullable=True, default=None)
     date_format: Mapped[str] = mapped_column(nullable=True, default=None)
-    send_date_in: Mapped[str] = mapped_column(nullable=True, default=None)
-    response_date_key: Mapped[str] = mapped_column(nullable=True, default="")
-    response_date_format: Mapped[str] = mapped_column(nullable=True, default="%Y-%m-%d")
+    send_date_in: Mapped[SendDateInEnum] = mapped_column(nullable=True, default=None)
+    response_date_key: Mapped[str] = mapped_column(nullable=False, default="")
+    response_date_format: Mapped[str] = mapped_column(
+        nullable=False, default="%Y-%m-%d"
+    )
--- dagster/src/schemas/qos.py
+++ dagster/src/schemas/qos.py
@@ -1,6 +1,9 @@
 import enum
+from collections.abc import Mapping
+from typing import Any
 
 from dagster import Config
+from dagster._config.config_type import ConfigScalar, ConfigScalarKind
 
 
 class AuthorizationTypeEnum(enum.Enum):
@@ -27,37 +30,45 @@ class SendQueryInEnum(enum.Enum):
     NONE = "NONE"
 
 
-# class ApiConfiguration(Config):
-#     id: str
-#     api_auth_api_key: str | None
-#     api_auth_api_value: str | None
-#     api_endpoint: str
-#     authorization_type: AuthorizationTypeEnum
-#     basic_auth_password: str | None
-#     basic_auth_username: str | None
-#     bearer_auth_bearer_token: str | None
-#     data_key: str
-#     date_created: str
-#     date_last_ingested: str
-#     date_last_successfully_ingested: str
-#     date_modified: str
-#     enabled: bool
-#     error_message: str | None
-#     page_number_key: str | None
-#     page_offset_key: str | None
-#     page_send_query_in: SendQueryInEnum
-#     page_size_key: str | None
-#     page_starts_with: int | None
-#     pagination_type: PaginationTypeEnum
-#     query_parameters: str | None
-#     request_body: str | None
-#     request_method: RequestMethodEnum
-#     school_id_key: str
-#     school_id_send_query_in: SendQueryInEnum
-#     size: int | None
+# class _DagsterConfigAuthorizationTypeEnum(type, ConfigScalar):
+#     def __init__(cls, name, bases, dct):
+#         type.__init__(cls, name, bases, dct)
+#         ConfigScalar.__init__(cls, key="authorization_type", given_name="AuthorizationTypeEnum", scalar_kind=ConfigScalarKind.STRING)
 
-#     class Config:
-#         orm_mode = True
+
+# class DagsterConfigAuthorizationType(AuthorizationTypeEnum, metaclass=_DagsterConfigAuthorizationTypeEnum):
+
+#     @classmethod
+#     def _make_authorization_type_from_string(cls, value):  # Self type return
+#         if isinstance(value, cls):
+#             return value
+#         if isinstance(value, str):
+#             return cls[value]
+#         raise ValueError  # your error message here
+
+#     @classmethod
+#     def __get_validators__(cls):
+#         yield cls._make_authorization_type_from_string
+
+
+class DagsterConfigAuthorizationType(ConfigScalar):
+    key = "authorization_type"
+    given_name = "AuthorizationTypeEnum"
+    scalar_kind = ConfigScalarKind.STRING
+
+    @classmethod
+    def _make_authorization_type_from_string(cls, value):  # Self type return
+        if isinstance(value, cls):
+            return value
+        if isinstance(value, str):
+            return AuthorizationTypeEnum[value]
+        if isinstance(value, AuthorizationTypeEnum):
+            return value
+        raise ValueError(f"this is wrong: {type(value)}")  # your error message here
+
+    @classmethod
+    def __get_validators__(cls):
+        yield cls._make_authorization_type_from_string
 
 
 class ApiConfiguration(Config):
@@ -65,7 +76,7 @@ class ApiConfiguration(Config):
     api_auth_api_key: str | None
     api_auth_api_value: str | None
     api_endpoint: str
-    authorization_type: str
+    authorization_type: DagsterConfigAuthorizationType
     basic_auth_password: str | None
     basic_auth_username: str | None
     bearer_auth_bearer_token: str | None
@@ -89,18 +100,77 @@ class ApiConfiguration(Config):
     school_id_send_query_in: str
     size: int | None
 
+    def _get_non_default_public_field_values_authorization_type(
+        self,
+    ) -> Mapping[str, Any]:
+        """Returns a dictionary representation of this config object,
+        ignoring any private fields, and any optional fields that are None.
+
+        Inner fields are returned as-is in the dictionary,
+        meaning any nested config objects will be returned as config objects, not dictionaries.
+        """
+
+        def map_authorization_type_field(field_key: str, value):
+            field = self.__fields__.get(field_key)
+            if field and issubclass(field.type_, DagsterConfigAuthorizationType):
+                return value.value
+            return value
+
+        return {
+            k: map_authorization_type_field(k, v)
+            for k, v in super()._get_non_none_public_field_values().items()
+        }
+
     class Config:
         orm_mode = True
 
 
+# class ApiConfiguration(Config):
+#     id: str
+#     api_auth_api_key: str | None
+#     api_auth_api_value: str | None
+#     api_endpoint: str
+#     authorization_type: str
+#     basic_auth_password: str | None
+#     basic_auth_username: str | None
+#     bearer_auth_bearer_token: str | None
+#     data_key: str
+#     date_created: str
+#     date_last_ingested: str
+#     date_last_successfully_ingested: str
+#     date_modified: str
+#     enabled: bool
+#     error_message: str | None
+#     page_number_key: str | None
+#     page_offset_key: str | None
+#     page_send_query_in: str
+#     page_size_key: str | None
+#     page_starts_with: int | None
+#     pagination_type: str
+#     query_parameters: str | None
+#     request_body: str | None
+#     request_method: str
+#     school_id_key: str
+#     school_id_send_query_in: str
+#     size: int | None
+
+#     class Config:
+#         orm_mode = True
+
+
 class SchoolConnectivityConfig(ApiConfiguration):
     ingestion_frequency_minutes: int
-    schema_url: str
+    schema_url: str | None
     school_list_id: str
+    date_key: str | None
+    date_format: str | None
+    send_date_in: str | None
+    response_date_key: str
+    response_date_format: str
 
 
 class SchoolListConfig(ApiConfiguration):
-    column_to_schema_mapping: str
+    column_to_schema_mapping: dict[str, str]
     name: str
     user_email: str
     user_id: str
