41b1ebe3be27ba4c3f7e67ee95bced21fe92b98b
tm-erin-cheng <erin@thinkingmachin.es>
2024-04-29 17:40:22 +0800

    fix: revert

--- dagster/src/schedule/datahub.py
+++ dagster/src/schedule/datahub.py
@@ -1,31 +1,21 @@
-from dagster import DefaultScheduleStatus, ScheduleDefinition
+from dagster import ScheduleDefinition
 from src.jobs.datahub import (
     datahub__ingest_coverage_notebooks_from_github_job,
     datahub__materialize_prerequisites_job,
     datahub__update_access_job,
 )
-from src.settings import settings
 
 datahub_materialize_prerequisities_schedule = ScheduleDefinition(
     job=datahub__materialize_prerequisites_job,
     cron_schedule="0 0 1 1 *",  # yearly
-    default_status=DefaultScheduleStatus.RUNNING
-    if settings.IN_PRODUCTION
-    else DefaultScheduleStatus.STOPPED,
 )
 
 datahub_update_access_schedule = ScheduleDefinition(
     job=datahub__update_access_job,
-    cron_schedule=settings.DEFAULT_SCHEDULE_CRON,
-    default_status=DefaultScheduleStatus.RUNNING
-    if settings.IN_PRODUCTION
-    else DefaultScheduleStatus.STOPPED,
+    cron_schedule="30 * * * *",
 )
 
 datahub_ingest_coverage_notebooks_schedule = ScheduleDefinition(
     job=datahub__ingest_coverage_notebooks_from_github_job,
     cron_schedule="0 0 1 * *",  # monthly
-    default_status=DefaultScheduleStatus.RUNNING
-    if settings.IN_PRODUCTION
-    else DefaultScheduleStatus.STOPPED,
 )
