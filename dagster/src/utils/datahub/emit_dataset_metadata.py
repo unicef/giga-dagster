b1864445d2386520eb5f133172e6eb5e996f65f8
tm-sofia-pineda <sofia.pineda@thinkingmachin.es>
2024-05-02 20:03:14 +0800

    fix: no column licenses after staging

--- dagster/src/utils/datahub/emit_dataset_metadata.py
+++ dagster/src/utils/datahub/emit_dataset_metadata.py
@@ -275,8 +275,15 @@ def datahub_emit_metadata_with_exception_catcher(
                 spark.spark_session,
                 config.metastore_schema,
             )
-            column_licenses = get_column_licenses(config=config)
-            context.log.info(column_licenses)
+            try:
+                column_licenses = get_column_licenses(config=config)
+                context.log.info(column_licenses)
+            except Exception as e:
+                context.log.warning(f"Cannot retrieve licenses: {e}")
+                context.log.warning(
+                    "No column licenses defined for datasets after staging."
+                )
+                pass
             add_column_metadata(
                 dataset_urn=config.datahub_destination_dataset_urn,
                 column_licenses=column_licenses,
