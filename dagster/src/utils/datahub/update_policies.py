f5b663170c8852e4a9e288c32c1fbe52d7b8bc0f
Sofia Anne Pineda <111489241+tm-sofia-pineda@users.noreply.github.com>
2024-05-06 23:43:08 +0800

    Fix/datahub rbac (#181)
    * fix: user group parsing issue
    
    * feat: add logs and sentry capture

--- dagster/src/utils/datahub/update_policies.py
+++ dagster/src/utils/datahub/update_policies.py
@@ -2,6 +2,7 @@ import json
 from urllib import parse
 
 import country_converter as cc
+import sentry_sdk
 from datahub.ingestion.graph.client import DatahubClientConfig, DataHubGraph
 
 from dagster import OpExecutionContext
@@ -10,6 +11,7 @@ from src.utils.datahub.builders import build_group_urn
 from src.utils.datahub.identify_country_name import identify_country_name
 from src.utils.logger import get_context_with_fallback_logger
 from src.utils.op_config import FileConfig
+from src.utils.sentry import log_op_context
 
 
 def policy_mutation_query(group_urn: str) -> str:
@@ -123,7 +125,7 @@ def update_policies(context: OpExecutionContext = None) -> None:
     )
     for group_urn in group_urns_iterator():
         update_policy_base(
-            group_urn=group_urn,
+            group_urn=parse.unquote(group_urn),
             datahub_graph_client=datahub_graph_client,
             context=context,
         )
@@ -168,7 +170,7 @@ def update_policy_base(
             datahub_graph_client.execute_graphql(query=query)
             logger.info("DATAHUB POLICY UPDATED SUCCESSFULLY.")
         except Exception as error:
-            logger.error(error)
+            logger.warning(error)
             try:
                 query = create_policy_query(group_urn=group_urn)
                 logger.info(f"CREATING DATAHUB POLICY: {group_urn}...")
@@ -177,8 +179,13 @@ def update_policy_base(
                 logger.info("DATAHUB POLICY UPDATED SUCCESSFULLY.")
             except Exception:
                 logger.error(error)
+                log_op_context(context)
+                sentry_sdk.capture_exception(error=error)
     else:
-        logger.error(f"INVALID COUNTRY NAME: {country_name}")
+        warning_message = f"INVALID COUNTRY NAME: {country_name}. No Datahub Policy is created/updated for this role."
+        logger.warning(warning_message)
+        log_op_context(context)
+        sentry_sdk.capture_message(warning_message)
 
 
 if __name__ == "__main__":
