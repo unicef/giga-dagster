c09be536abdabc63282acef7e16c12889566b174
tm-sofia-pineda <sofia.pineda@thinkingmachin.es>
2024-05-07 19:01:24 +0800

    feat: create separate job and sensor for generate silver

--- dagster/src/jobs/adhoc.py
+++ dagster/src/jobs/adhoc.py
@@ -2,12 +2,12 @@ from dagster import define_asset_job
 
 school_master__convert_gold_csv_to_deltatable_job = define_asset_job(
     name="school_master__convert_gold_csv_to_deltatable_job",
-    selection="adhoc__load_master_csv*",
+    selection="*adhoc__broadcast_master_release_notes",
 )
 
 school_reference__convert_gold_csv_to_deltatable_job = define_asset_job(
     name="school_reference__convert_gold_csv_to_deltatable_job",
-    selection="adhoc__load_reference_csv*",
+    selection="*adhoc__publish_reference_to_gold",
 )
 
 school_qos__convert_csv_to_deltatable_job = define_asset_job(
@@ -19,4 +19,9 @@ school_qos__convert_csv_to_deltatable_job = define_asset_job(
 school_master__generate_mock_table_cdf_job = define_asset_job(
     name="school_master__generate_mock_table_cdf_job",
     selection="*adhoc__generate_v3",
+)
+
+school_master__generate_silver_job = define_asset_job(
+    name="school_master__generate_silver_job",
+    selection=["adhoc__generate_silver_geolocation", "adhoc__generate_silver_coverage"],
 )
