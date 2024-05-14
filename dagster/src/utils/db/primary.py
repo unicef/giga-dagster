3a6c8e2b79f2f8d0b946ebb3a2674e251a2951d3
Kenneth V. Domingo <kenneth@thinkingmachin.es>
2024-05-15 00:36:24 +0800

    feat: db interface for mlab & proco

--- dagster/src/utils/db/primary.py
+++ dagster/src/utils/db/primary.py
@@ -1,46 +1,9 @@
-from collections.abc import Generator
-from contextlib import AbstractContextManager, contextmanager
-from typing import Any
-
-from loguru import logger
-from sqlalchemy import create_engine
-from sqlalchemy.exc import DatabaseError
-from sqlalchemy.orm import Session, sessionmaker
-
 from src.settings import settings
 
-engine = create_engine(
-    settings.INGESTION_DATABASE_URL,
-    echo=not settings.IN_PRODUCTION,
-    future=True,
-)
+from .base import PostgresDatabaseProvider
 
-session_maker = sessionmaker(
-    bind=engine,
-    autoflush=True,
-    autocommit=False,
-    expire_on_commit=False,
-)
+_primary = PostgresDatabaseProvider(settings.INGESTION_DATABASE_URL)
 
+get_db = _primary.get_db
 
-def get_db() -> Generator[Session, Any, Any]:
-    session = session_maker()
-    try:
-        yield session
-    except DatabaseError as err:
-        logger.error(str(err))
-        raise err
-    finally:
-        session.close()
-
-
-@contextmanager
-def get_db_context() -> AbstractContextManager[Session]:
-    session = session_maker()
-    try:
-        yield session
-    except DatabaseError as err:
-        logger.error(str(err))
-        raise err
-    finally:
-        session.close()
+get_db_context = _primary.get_db_context
