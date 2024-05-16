67e6905b26d1649e4ba16995b744db7ba3e46f4e
Kenneth V. Domingo <kenneth@thinkingmachin.es>
2024-05-16 12:49:18 +0800

    fix: mlab/proco debug queries

--- dagster/src/assets/debug/assets.py
+++ dagster/src/assets/debug/assets.py
@@ -1,6 +1,6 @@
 from dagster_pyspark import PySparkResource
 from pyspark.sql import SparkSession
-from sqlalchemy import select, text
+from sqlalchemy import text
 
 from dagster import Config, MetadataValue, OpExecutionContext, Output, asset
 
@@ -37,41 +37,74 @@ def debug__drop_table(
 
 @asset
 def debug__test_mlab_db_connection(context: OpExecutionContext):
-    from models.connectivity_rt import MlabSchools
-    from src.schemas.connectivity_rt import MlabSchools as MlabSchoolsSchema
     from src.utils.db.mlab import get_db_context
 
     with get_db_context() as db:
         context.log.info(db.execute(text("SELECT 1")))
-        res = db.scalars(select(MlabSchools).limit(10)).all()
-        res = [MlabSchoolsSchema.from_orm(r) for r in res]
+        res = db.scalars(
+            text("""
+        SELECT
+            DISTINCT mlab.school_id school_id_govt,
+            (min(mlab."timestamp") over (partition by mlab.school_id))::DATE mlab_created_date,
+            client_info::JSON ->> 'Country' country_code,
+            'mlab' source
+        FROM public.measurements mlab
+        LIMIT 10
+        """)
+        ).all()
+        res = [
+            {k: v for k, v in r.__dict__.items() if k != "_sa_instance_state"}
+            for r in res
+        ]
 
     return Output(None, metadata={"mlab_schools": MetadataValue.json(res)})
 
 
 @asset
 def debug__test_proco_db_connection(context: OpExecutionContext):
-    from models.connectivity_rt import GigaMeterSchools, RTSchools
-    from src.schemas.connectivity_rt import (
-        GigaMeterSchools as GigaMeterSchoolsSchema,
-        RTSchools as RTSchoolsSchema,
-    )
     from src.utils.db.proco import get_db_context
 
     with get_db_context() as db:
         context.log.info(db.execute(text("SELECT 1")))
-        res = db.scalars(select(GigaMeterSchools).limit(10)).all()
-        giga_meter_schools = [GigaMeterSchoolsSchema.from_orm(r) for r in res]
+        res = db.scalars(
+            text("""
+        SELECT
+            DISTINCT sch.giga_id_school school_id_giga,
+            sch.external_id school_id_govt,
+            (min(stat.created) over (partition by stat.school_id)) connectivity_RT_ingestion_timestamp,
+            c.code country_code,
+            c.name country
+        FROM connection_statistics_schooldailystatus stat
+        LEFT JOIN schools_school sch ON sch.id = stat.school_id
+        LEFT JOIN locations_country c ON c.id = sch.country_id
+        LIMIT 10
+        """)
+        ).all()
+        rt_schools = [
+            {k: v for k, v in r.__dict__.items() if k != "_sa_instance_state"}
+            for r in res
+        ]
 
-        res = db.scalars(select(RTSchools).limit(10)).all()
-        rt_schools = [RTSchoolsSchema.from_orm(r) for r in res]
+        res = db.scalars(
+            text("""
+        SELECT
+            DISTINCT dca.giga_id_school school_id_giga,
+            school_id school_id_govt,
+            'daily_checkapp' source,
+        FROM dailycheckapp_measurements dca
+        WHERE dca.giga_id_school !=''
+        LIMIT 10
+        """)
+        ).all()
+        giga_meter_schools = [
+            {k: v for k, v in r.__dict__.items() if k != "_sa_instance_state"}
+            for r in res
+        ]
 
     return Output(
         None,
         metadata={
-            "giga_meter_schools": MetadataValue.json(
-                [g.dict() for g in giga_meter_schools]
-            ),
-            "rt_schools": MetadataValue.json([r.dict() for r in rt_schools]),
+            "giga_meter_schools": MetadataValue.json(giga_meter_schools),
+            "rt_schools": MetadataValue.json(rt_schools),
         },
     )
