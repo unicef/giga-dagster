faacdffbc9931fd7080d21da9595e648692d8630
tm-sofia-pineda <sofia.pineda@thinkingmachin.es>
2024-05-21 14:05:37 +0800

    refactor: job use asset grouping

--- dagster/src/jobs/unstructured.py
+++ dagster/src/jobs/unstructured.py
@@ -1,8 +1,7 @@
-from dagster import define_asset_job
+from dagster import AssetSelection, define_asset_job
+from src.assets.unstructured import GROUP_NAME as UNSTRUCTURED_DATA_GROUP_NAME
 
 unstructured__emit_metadata_to_datahub_job = define_asset_job(
     name="unstructured__emit_metadata_to_datahub_job",
-    selection=[
-        "unstructured_raw",
-    ],
+    selection=AssetSelection.groups(UNSTRUCTURED_DATA_GROUP_NAME),
 )
