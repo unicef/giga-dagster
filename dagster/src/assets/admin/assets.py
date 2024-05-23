8f683e65f194f403b7fcc764c04ec4dfa433ea5d
Kenneth V. Domingo <kenneth@thinkingmachin.es>
2024-05-23 18:21:49 +0800

    feat: add rollback admin asset

--- dagster/src/assets/admin/assets.py
+++ dagster/src/assets/admin/assets.py
@@ -1,4 +1,17 @@
-from dagster import DagsterRunStatus, OpExecutionContext, RunsFilter, asset
+from dagster_pyspark import PySparkResource
+from delta import DeltaTable
+from pydantic import conint
+from pyspark.sql import SparkSession
+
+from dagster import (
+    Config,
+    DagsterRunStatus,
+    MetadataValue,
+    OpExecutionContext,
+    Output,
+    RunsFilter,
+    asset,
+)
 
 
 @asset
@@ -23,3 +36,25 @@ def admin__terminate_all_runs(context: OpExecutionContext):
         context.log.info(f"Terminated run {run.job_name} ({run.run_id})")
 
     context.log.info(f"Terminated {len(runs)} run(s)")
+
+
+class RollbackTableConfig(Config):
+    schema_name: str
+    table_name: str
+    version: conint(ge=0)
+
+
+@asset
+def admin__rollback_table_version(
+    _: OpExecutionContext, config: RollbackTableConfig, spark: PySparkResource
+):
+    s: SparkSession = spark.spark_session
+    dt = DeltaTable.forName(s, f"{config.schema_name}.{config.table_name}")
+    result = dt.restoreToVersion(config.version)
+
+    return Output(
+        None,
+        metadata={
+            "result": MetadataValue.md(result.toPandas().to_markdown()),
+        },
+    )
