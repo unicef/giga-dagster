65c13033361bd5ac3940a91b4962ca2efed81aeb
Sofia Anne Pineda <111489241+tm-sofia-pineda@users.noreply.github.com>
2024-06-04 20:47:16 +0800

    fix: search quer - also include soft deleted (#260)

--- dagster/src/utils/datahub/list_datasets.py
+++ dagster/src/utils/datahub/list_datasets.py
@@ -1,10 +1,11 @@
+from datahub.ingestion.graph.filters import RemovedStatusFilter
+
 from src.utils.datahub.graphql import datahub_graph_client as emitter
 
 
 def list_datasets_by_filter(search_value: str) -> list:
     dataset_urns_iterator = emitter.get_urns_by_filter(
-        entity_types=["dataset"],
-        query=search_value,
+        entity_types=["dataset"], query=search_value, status=RemovedStatusFilter.ALL
     )
     return list(dataset_urns_iterator)
 
