c5b88508d649f0a4839c21f2e61e485b2d9c4681
Kenneth V. Domingo <kenneth@thinkingmachin.es>
2024-08-15 20:46:13 +0800

    feat: add failure reason; sort by critical failures first

--- dagster/src/data_quality_checks/critical.py
+++ dagster/src/data_quality_checks/critical.py
@@ -20,48 +20,87 @@ def critical_error_checks(
 
     # all mandatory columns included in critical error checks
     critial_column_dq_checks = [
-        f.col(f"dq_is_null_mandatory-{column}") for column in config_column_list
+        f"dq_is_null_mandatory-{column}" for column in config_column_list
     ]
 
     # other critical checks per dataset
     if dataset_type in ["master", "geolocation"]:
         critial_column_dq_checks.extend(
             [
-                f.col("dq_duplicate-school_id_govt"),
-                f.col("dq_duplicate-school_id_giga"),
-                f.col("dq_is_invalid_range-latitude"),
-                f.col("dq_is_invalid_range-longitude"),
-                f.col("dq_is_not_within_country"),
+                "dq_duplicate-school_id_govt",
+                "dq_duplicate-school_id_giga",
+                "dq_is_invalid_range-latitude",
+                "dq_is_invalid_range-longitude",
+                "dq_is_not_within_country",
             ]
         )
         if mode == "create":
-            critial_column_dq_checks.extend(
-                [
-                    f.col("dq_is_not_create"),
-                ]
-            )
+            critial_column_dq_checks.append("dq_is_not_create")
         elif mode == "update":
-            critial_column_dq_checks.extend(
-                [
-                    f.col("dq_is_not_update"),
-                ]
-            )
+            critial_column_dq_checks.append("dq_is_not_update")
     elif dataset_type in ["reference", "coverage", "coverage_fb", "coverage_itu"]:
-        critial_column_dq_checks.extend([f.col("dq_duplicate-school_id_giga")])
+        critial_column_dq_checks.append("dq_duplicate-school_id_giga")
     elif dataset_type == "qos":
         critial_column_dq_checks.extend(
             [
-                f.col("dq_duplicate-school_id_giga"),
-                # f.col("dq_duplicate-school_id_govt"),
+                "dq_duplicate-school_id_giga",
+                # "dq_duplicate-school_id_govt",
             ]
         )
 
-    df = df.withColumn(
+    df = df.withColumns(
+        {
+            "dq_has_critical_error": f.when(
+                reduce(add, [f.col(c) for c in critial_column_dq_checks]) > 0,
+                1,
+            ).otherwise(0),
+            "failure_reason": f.concat_ws(
+                ", ",
+                *[
+                    f.when(
+                        f.col(c) == 1,
+                        f.lit(c[3:]),  # remove `dq_` prefix
+                    )
+                    for c in critial_column_dq_checks
+                ],
+            ),
+        }
+    )
+
+    rest_columns = df.columns
+    dataset_has_school_id_giga = "school_id_giga" in rest_columns
+    dataset_has_school_id_govt = "school_id_govt" in rest_columns
+
+    for col in [
         "dq_has_critical_error",
-        f.when(
-            reduce(add, critial_column_dq_checks) > 0,
-            1,
-        ).otherwise(0),
+        "failure_reason",
+        "school_id_giga",
+        "school_id_govt",
+    ]:
+        if col in rest_columns:
+            idx = rest_columns.index(col)
+            rest_columns.pop(idx)
+
+    ordered_columns = []
+    order_by_columns = []
+
+    if dataset_has_school_id_giga:
+        ordered_columns.append("school_id_giga")
+        order_by_columns.append("school_id_giga")
+    if dataset_has_school_id_govt:
+        ordered_columns.append("school_id_govt")
+        order_by_columns.append("school_id_govt")
+
+    ordered_columns.extend(
+        [
+            "dq_has_critical_error",
+            "failure_reason",
+            *rest_columns,
+        ]
+    )
+    df = df.select(*ordered_columns).orderBy(
+        f.col("dq_has_critical_error").desc(),
+        *order_by_columns,
     )
 
     return df
