b7fd32c3a6ce7bbfdc2a468ce059d32364a9619f
Gerlito Chagas <gerlito.chagas@gmail.com>
2024-10-30 13:44:46 +0800

    feat: add nullability constraints to sync_schema

--- dagster/src/utils/delta.py
+++ dagster/src/utils/delta.py
@@ -266,10 +266,13 @@ def build_nullability_queries(
             if match_.nullable != column.nullable:
                 if match_.nullable:
                     alter_stmts.append(
-                        f"ALTER TABLE {table_name} ALTER COLUMN {column.name} DROP NOT NULL"
+                        f"ALTER TABLE {table_name} ADD CONSTRAINT {column.name}_not_null CHECK ({column.name} is not null)"
                     )
 
                 else:
+                    alter_stmts.append(
+                        f"ALTER TABLE {table_name} DROP CONSTRAINT IF EXISTS {column.name}_not_null"
+                    )
                     alter_stmts.append(
                         f"ALTER TABLE {table_name} ALTER COLUMN {column.name} SET NOT NULL"
                     )
