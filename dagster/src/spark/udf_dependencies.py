fcf0fc9a4aa47ae2fd89ebfff2a186a260fe98fa
sharky93 <rishabhr123@gmail.com>
2025-01-13 19:23:52 +0530

    fixes handling for when input geometry is empty

--- dagster/src/spark/udf_dependencies.py
+++ dagster/src/spark/udf_dependencies.py
@@ -96,13 +96,15 @@ def is_within_boundary_distance(
 
     point = get_point(longitude, latitude)
     if point is not None and geometry is not None:
-        p1, _ = nearest_points(geometry, point)
+        if geometry.is_empty or (isinstance(point, Point) and point.is_empty):
+            return False
 
-        # geodesic distance format is in lat-long, we need to switch these over!
-        point1 = p1.coords[0]
-        point1 = (point1[1], point1[0])
-        point2 = (latitude, longitude)
         try:
+            p1, _ = nearest_points(geometry, point)
+            # geodesic distance format is in lat-long, we need to switch these over!
+            point1 = p1.coords[0]
+            point1 = (point1[1], point1[0])
+            point2 = (latitude, longitude)
             distance = geodesic(point1, point2).km
         except ValueError:
             return False
