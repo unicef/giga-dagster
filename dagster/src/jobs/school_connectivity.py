1402446c048056692e39510a924fbd1e05cdf889
Brian Musisi <brianmusisi@berkeley.edu>
2025-02-04 22:59:46 +0300

    chore: use correct job name in sensor

--- dagster/src/jobs/school_connectivity.py
+++ dagster/src/jobs/school_connectivity.py
@@ -10,6 +10,6 @@ school_connectivity__new_realtime_schools_job = define_asset_job(
 
 school_connectivity__update_schools_realtime_status_job = define_asset_job(
     name="school_connectivity__update_schools_realtime_status_job",
-    selection=["school_connectivity_update_connected_schools_table*"],
+    selection=["school_connectivity_update_realtime_schools_status*"],
     tags={"dagster/max_runtime": settings.DEFAULT_MAX_RUNTIME},
 )
