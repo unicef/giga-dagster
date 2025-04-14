59276669c315468e9534f60e1a3acbafb0477713
Brian Musisi <brianmusisi@berkeley.edu>
2025-04-14 09:25:56 +0200

    fix: correct file import

--- dagster/src/resources/__init__.py
+++ dagster/src/resources/__init__.py
@@ -3,8 +3,8 @@ from enum import Enum
 from src.utils.adls import ADLSFileClient
 from src.utils.spark import pyspark
 
-from .io.managers.adls_generic_file import ADLSGenericFileIOManager
 from .io_managers.adls_delta import ADLSDeltaIOManager
+from .io_managers.adls_generic_file import ADLSGenericFileIOManager
 from .io_managers.adls_json import ADLSJSONIOManager
 from .io_managers.adls_pandas import ADLSPandasIOManager
 from .io_managers.adls_passthrough import ADLSPassthroughIOManager
