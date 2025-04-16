b8f8edbeff9fda7011a0306574d70a799d690b6d
Rishabh Raj <sharky93@users.noreply.github.com>
2025-04-17 03:22:37 +0530

    Update superset.py

--- dagster/src/schedule/superset.py
+++ dagster/src/schedule/superset.py
@@ -4,5 +4,5 @@ from ..jobs.superset import refresh_table
 
 superset_schedule = ScheduleDefinition(
     job=refresh_table,
-    cron_schedule="0 6 * * *",  # Every day at 9 AM
+    cron_schedule="0 5 * * *",
 )
