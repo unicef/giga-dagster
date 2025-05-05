6a7763f723f12e1d0bc1ab0a2dbf0b9df313065a
Jericho Arcelao <echo@thinkingmachin.es>
2025-05-05 10:17:45 +0000

    fix: linter errors

--- dagster/src/jobs/superset.py
+++ dagster/src/jobs/superset.py
@@ -1,6 +1,9 @@
-from dagster import OpExecutionContext, job, op
 import time
-from ..resources.superset import get_access_token, fetch_saved_query, run_query
+
+from dagster import OpExecutionContext, job, op
+
+from ..resources.superset import fetch_saved_query, get_access_token, run_query
+
 
 @op
 def fetch_and_run_query(context: OpExecutionContext):
@@ -11,11 +14,11 @@ def fetch_and_run_query(context: OpExecutionContext):
         for table in response:
             query_to_delete = {
                 "label": "Query to Delete",
-                "sql": table["Query to Delete"]
+                "sql": table["Query to Delete"],
             }
             query_to_create = {
                 "label": "Query to Create",
-                "sql": table["Query to Create"]
+                "sql": table["Query to Create"],
             }
             context.log.info(f"running query: {table['Title']}")
             run_query(query_to_delete, access_token)
