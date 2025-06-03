4566aa57d5604e44aafabf3d0cd38855d40f9020
Gerlito Chagas <gerlito.chagas@gmail.com>
2025-06-03 14:39:02 +0800

    refactor: add docs, rename emitter to datahub_graph_client

--- dagster/src/utils/datahub/list_datasets.py
+++ dagster/src/utils/datahub/list_datasets.py
@@ -1,10 +1,10 @@
 from datahub.ingestion.graph.filters import RemovedStatusFilter
 
-from src.utils.datahub.graphql import datahub_graph_client as emitter
+from src.utils.datahub.graphql import datahub_graph_client
 
 
 def list_datasets_by_filter(search_value: str) -> list:
-    dataset_urns_iterator = emitter.get_urns_by_filter(
+    dataset_urns_iterator = datahub_graph_client.get_urns_by_filter(
         entity_types=["dataset"], query=search_value, status=RemovedStatusFilter.ALL
     )
     return list(dataset_urns_iterator)
@@ -13,4 +13,4 @@ def list_datasets_by_filter(search_value: str) -> list:
 if __name__ == "__main__":
     qos_list = list_datasets_by_filter("qos")
     print(qos_list[0])
-    emitter.soft_delete_entity(qos_list[0])
+    datahub_graph_client.soft_delete_entity(qos_list[0])
