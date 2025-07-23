e38c9c38dc74f5f5a49bd2a2e1c560022ab4b29d
Brian Musisi <brianmusisi@berkeley.edu>
2025-07-23 16:12:21 +0200

    fix: simplify parameters in downstream functions

--- dagster/src/utils/nocodb/get_nocodb_data.py
+++ dagster/src/utils/nocodb/get_nocodb_data.py
@@ -4,16 +4,14 @@ import requests
 from src.settings import settings
 
 
-def get_nocodb_table_rows(view_id, table_id, offset=0, limit=100, where=""):
+def get_nocodb_table_rows(table_id, offset=0, limit=100):
     """
     Retrieve rows from a specified NoCoDB table and returns them as a list of dictionaries
 
     Args:
-        view_id (str): The ID of the NocoDB view to query.
         table_id (str): The ID of the NocoDB table to query.
         offset (int, optional): Number of rows to skip before starting to return rows. Defaults to 0.
         limit (int, optional): Maximum number of rows to return. Defaults to 500.
-        where (str, optional): SQL-like WHERE clause to filter the results. Defaults to empty string.
 
     Returns:
         list[dict]: A list of dictionaries representing the rows in the table.
@@ -30,7 +28,7 @@ def get_nocodb_table_rows(view_id, table_id, offset=0, limit=100, where=""):
     # loop through and get all the data in the table
     while not is_last_page:
         # set the parameters to query the current page of data
-        params = {"offset": offset, "limit": limit, "where": where, "viewID": view_id}
+        params = {"offset": offset, "limit": limit}
 
         # fetch the data from the api
         response = requests.get(table_url, params=params, headers=headers)
@@ -47,18 +45,12 @@ def get_nocodb_table_rows(view_id, table_id, offset=0, limit=100, where=""):
     return
 
 
-def get_nocodb_table_as_pandas_dataframe(
-    view_id, table_id, offset=0, limit=500, where=""
-):
+def get_nocodb_table_as_pandas_dataframe(table_id):
     """
     Retrieve data from a NoCoDB table and convert it to a pandas DataFrame.
 
     Args:
-        view_id (str): The ID of the NoCoDB view to query.
         table_id (str): The ID of the NoCoDB table to query.
-        offset (int, optional): Number of rows to skip before starting to return rows. Defaults to 0.
-        limit (int, optional): Maximum number of rows to return. Defaults to 500.
-        where (str, optional): SQL-like WHERE clause to filter the results. Defaults to empty string.
 
     Returns:
         pandas.DataFrame: A DataFrame containing the table data, with empty rows removed.
@@ -69,30 +61,22 @@ def get_nocodb_table_as_pandas_dataframe(
         (i.e., df.iloc[:, 3:]).
     """
 
-    rows_list = get_nocodb_table_rows(
-        view_id, table_id, offset=offset, limit=limit, where=where
-    )
+    rows_list = get_nocodb_table_rows(table_id)
     df = pd.DataFrame(rows_list)
     df = df[df.iloc[:, 3:].notna().any(axis="columns")]
     return df
 
 
-def get_nocodb_table_as_key_value_mapping(
-    view_id, table_id, key_column=None, value_column=None, offset=0, limit=500, where=""
-):
+def get_nocodb_table_as_key_value_mapping(table_id, key_column=None, value_column=None):
     """
     Convert a NoCoDB table into a mapping of key-value pairs. If columns are not explicitly provided,
     it defaults to using the 4th and 5th columns (index-based) from the fetched data rows.
     Only non-empty key-value pairs are included in the resulting mapping.
 
     Args:
-        view_id (str): The ID of the NoCoDB view to query.
         table_id (str): The ID of the NoCoDB table to query.
         key_column (str, optional): Name of the column to use as the key. Defaults to None.
         value_column (str, optional): Name of the column to use as the value. Defaults to None.
-        offset (int, optional): Number of rows to skip before starting to return rows. Defaults to 0.
-        limit (int, optional): Maximum number of rows to return. Defaults to 500.
-        where (str, optional): SQL-like WHERE clause to filter the results. Defaults to empty string.
 
     Returns:
     dict: key-value mapping derived from the specified table and columns.
@@ -105,9 +89,7 @@ def get_nocodb_table_as_key_value_mapping(
         raise TypeError(
             "Either both key_column and value_column must be provided or neither"
         )
-    rows_list = get_nocodb_table_rows(
-        view_id, table_id, offset=offset, limit=limit, where=where
-    )
+    rows_list = get_nocodb_table_rows(table_id)
 
     if not (key_column and value_column):
         columns = list(rows_list[0].keys())
