b314704f6f6780167ee969e2774f77eb2d31fdbc
sharky93 <rishabhr123@gmail.com>
2025-08-11 12:22:40 +0530

    update periodic refresh for physical table

--- dagster/src/jobs/superset.py
+++ dagster/src/jobs/superset.py
@@ -2,12 +2,19 @@ import time
 
 from dagster import OpExecutionContext, job, op
 
-from ..resources.superset import fetch_saved_query, get_access_token, run_query
+from ..resources.superset import fetch_saved_query, get_access_token, refresh_access_token, run_query
 
 
 @op
 def fetch_and_run_query(context: OpExecutionContext):
-    access_token = get_access_token()
+    access_token = None
+    refresh_token = None
+    auth_data = get_access_token()
+
+    if auth_data:
+        access_token = auth_data.get("access_token")
+        refresh_token = auth_data.get("refresh_token")
+
     if access_token:
         context.log.info("successfully authenticated with Superset")
         response = fetch_saved_query()
@@ -30,6 +37,14 @@ def fetch_and_run_query(context: OpExecutionContext):
             context.log.info(f"status code: {x.status_code}")
             context.log.info(f"response: {x.text}")
             time.sleep(90)
+            response = refresh_access_token(refresh_token)
+            if response.status_code == 200:
+                auth_data = response.json()
+                access_token = auth_data.get("access_token")
+            else:
+                context.log.error(
+                    "error in authenticating with Superset so unable to run the query now"
+                )
     else:
         context.log.error(
             "error in authenticating with Superset so this will be ignored now"
