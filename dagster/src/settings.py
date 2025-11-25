4cd4a74a3f7855b6e6bc2dd75b0095a907b3731b
tm-jay-lorenzo <jay.l@thinkingmachin.es>
2025-11-25 22:37:07 +0800

    test: remove allocations to test optimizations

--- dagster/src/settings.py
+++ dagster/src/settings.py
@@ -52,10 +52,7 @@ class Settings(BaseSettings):
     GITHUB_ACCESS_TOKEN: str = ""
     INGESTION_DB_PORT: int = 5432
     SPARK_DRIVER_CORES: int = 2
-    SPARK_DRIVER_MEMORY_MB: int = 5120  # ~5GB, leaving ~1GB for OS/overhead
-    SPARK_EXECUTOR_CORES: int = 2
-    SPARK_EXECUTOR_MEMORY_MB: int = 5120  # ~5GB per executor
-    SPARK_MAX_CORES: int = 6  # Total cores: worker_count × executor_cores
+    SPARK_DRIVER_MEMORY_MB: int = 1365
     LICENSE_LIST: list[str] = ["Open Source", "Giga Analysis", "CC-BY-4.0"]
     WAREHOUSE_USERNAME: str = ""
     LAKEHOUSE_USERNAME: str = ""
