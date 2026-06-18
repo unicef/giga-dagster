f7d693e13b23f390d62ef9c4694005a5dc63e983
Javiershenbc <jashen@unicef.org>
2026-06-18 13:20:24 +0200

    chore: fix ruff checks
    ruff format check fix

--- dagster/src/schedule/superset.py
+++ dagster/src/schedule/superset.py
@@ -2,7 +2,6 @@ from dagster import ScheduleDefinition
 
 from ..jobs.incremental import refresh_incremental_table
 from ..jobs.superset import refresh_table
-from ..jobs.incremental import refresh_incremental_table
 
 superset_schedule = ScheduleDefinition(
     job=refresh_table,
